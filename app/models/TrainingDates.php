c783f1936e4b5e880be01a999b131b7bf930010e
Michal Špaček <mail@michalspacek.cz>
2012-07-01 19:12:22 +0000

    Add models for training dates and trainings itself.

---
+++ app/models/TrainingDates.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Training dates model.
+ *
+ * @author     Michal Špaček
+ * @package    michalspacek.cz
+ */
+class TrainingDates extends BaseModel
+{
+	const TABLE_NAME = 'training_dates';
+}
---
+++ app/models/Trainings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Trainings model.
+ *
+ * @author     Michal Špaček
+ * @package    michalspacek.cz
+ */
+class Trainings extends BaseModel
+{
+	const TABLE_NAME = 'trainings';
+}
