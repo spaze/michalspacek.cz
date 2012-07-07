e8cee21134aad5f25d14836e5db6fc60ee6927cc
Michal Špaček <mail@michalspacek.cz>
2012-07-07 03:51:51 +0000

    Read past trainings done by yours truly also from database. Jakub's trainings stay hardcoded in the code, though, no need to have them in database as they will not change, it's history afterall.

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -7,115 +7,13 @@
  */
 abstract class BasePresenter extends \Nette\Application\UI\Presenter
 {
-	const TRAINING_PHP = 1;
-	const TRAINING_PHP5 = 2;
-	const TRAINING_SECURITY = 3;
-	const TRAINING_PERFORMANCE = 4;
 
-	/**
-	 * Training id to action mapping.
-	 *
-	 * @var array
-	 */
-	protected $trainings = array(
-		self::TRAINING_PHP => array(
-			'action' => 'uvodDoPhp',
-			'name' => 'Úvod do PHP',
-			'date' => null,  // or YYYY-MM-DD
-			'tentative' => 'září 2012',  // or null
-			'placeName' => 'Internet Info',
-			'placeUrl' => 'http://www.skolici-mistnost.cz/',
-			'placeAddress' => 'Milady Horákové 109/116, Praha 6',
-			'originalUrl' => 'http://php.vrana.cz/skoleni-uvod-do-php.php',
-			'pastTrainings' => array(
-				'spacek' => array(
-					'2012-05-09',
-					'2011-12-07',
-				),
-				'vrana' => array(
-					'2011-09-07', '2011-04-20',
-					'2010-12-01', '2010-03-02',
-					'2009-12-01', '2009-09-14', '2009-06-25', '2009-04-22', '2009-01-20',
-					'2008-12-02', '2008-10-13', '2008-02-29',
-					'2007-10-25', '2007-02-26',
-				),
-			),
-		),
-		self::TRAINING_PHP5 => array(
-			'action' => 'programovaniVPhp5',
-			'name' => 'Programování v PHP 5',
-			'date' => null,  // or YYYY-MM-DD
-			'tentative' => 'září 2012',  // or null
-			'placeName' => 'Internet Info',
-			'placeUrl' => 'http://www.skolici-mistnost.cz/',
-			'placeAddress' => 'Milady Horákové 109/116, Praha 6',
-			'originalUrl' => 'http://php.vrana.cz/skoleni-programovani-v-php-5.php',
-			'pastTrainings' => array(
-				'spacek' => array(
-					'2012-05-10',
-					'2011-12-08',
-				),
-				'vrana' => array(
-					'2011-09-08', '2011-04-21',
-					'2010-12-02', '2010-06-08', '2010-03-03',
-					'2009-12-02', '2009-09-29', '2009-09-15', '2009-06-26', '2009-04-23', '2009-01-21',
-					'2008-12-03', '2008-10-14', '2008-04-08',
-					'2007-10-26',
-					'2006-11-16', '2006-06-12',
-				),
-			),
-		),
-		self::TRAINING_SECURITY => array(
-			'action' => 'bezpecnostPhpAplikaci',
-			'name' => 'Bezpečnost PHP aplikací',
-			'date' => null,  // or YYYY-MM-DD
-			'tentative' => 'září 2012',  // or null
-			'placeName' => 'Classic 7, budova N',
-			'placeUrl' => 'http://www.classic7.cz/cs/faze-1/lokalita',
-			'placeAddress' => 'Jankovcova 1037/49, Praha 7',
-			'originalUrl' => 'http://php.vrana.cz/skoleni-bezpecnost-php-aplikaci.php',
-			'pastTrainings' => array(
-				'spacek' => array(
-					'2012-05-11',
-					'2011-12-09',
-				),
-				'vrana' => array(
-					'2011-09-16', '2011-09-05', '2011-04-29',
-					'2010-12-09', '2010-10-08', '2010-06-11', '2010-03-12', '2010-03-09',
-					'2009-12-08', '2009-09-17', '2009-06-08', '2009-03-12', '2009-03-10',
-					'2008-12-08', '2008-10-21', '2008-06-24', '2008-02-28', '2008-02-25',
-					'2007-10-29', '2007-10-23', '2007-06-26', '2007-04-16',
-					'2006-10-27', '2006-06-22', '2006-04-25',
-				),
-			),
-		),
-		self::TRAINING_PERFORMANCE => array(
-			'action' => 'vykonnostWebovychAplikaci',
-			'name' => 'Výkonnost webových aplikací',
-			'date' => null,  // or YYYY-MM-DD
-			'tentative' => 'září 2012',  // or null
-			'placeName' => 'Classic 7, budova N',
-			'placeUrl' => 'http://www.classic7.cz/cs/faze-1/lokalita',
-			'placeAddress' => 'Jankovcova 1037/49, Praha 7',
-			'originalUrl' => 'http://php.vrana.cz/skoleni-vykonnost-webovych-aplikaci.php',
-			'pastTrainings' => array(
-				'spacek' => array(
-					'2012-05-14',
-				),
-				'vrana' => array(
-					'2011-09-14', '2011-04-27',
-					'2010-12-07', '2010-03-10',
-					'2009-09-21', '2009-03-11',
-					'2008-12-09', '2008-10-22', '2008-06-27',
-				),
-			),
-		),
-	);
 
 	public function beforeRender()
 	{
 		$this->template->debugMode = $this->context->parameters['debugMode'];
 	}
+
 
 	protected function createTemplate($class = null)
 	{
@@ -123,4 +21,6 @@ abstract class BasePresenter extends \Nette\Application\UI\Presenter
 		$template->registerHelperLoader(callback(new \Bare\Nette\Templating\Helpers($this->getContext()), 'loader'));
 		return $template;
 	}
+
+
 }
--- app/presenters/SkoleniPresenter.php
+++ app/presenters/SkoleniPresenter.php
@@ -11,25 +11,70 @@ use \Nette\Application\UI\Form,
 class SkoleniPresenter extends BasePresenter
 {
 	/**
-	 * Currently processed training.
+	 * Currently processed training info.
 	 *
-	 * @var integer
+	 * @var boolean
 	 */
-	private $trainingId;
+	private $tentative;
+
+
+	/**
+	 * Past trainings done by Jakub.
+	 *
+	 * @var array
+	 */
+	protected $pastTrainingsJakub = array(
+		'uvodDoPhp' => array(
+			'2011-09-07', '2011-04-20',
+			'2010-12-01', '2010-03-02',
+			'2009-12-01', '2009-09-14', '2009-06-25', '2009-04-22', '2009-01-20',
+			'2008-12-02', '2008-10-13', '2008-02-29',
+			'2007-10-25', '2007-02-26',
+		),
+		'programovaniVPhp5' => array(
+			'2011-09-08', '2011-04-21',
+			'2010-12-02', '2010-06-08', '2010-03-03',
+			'2009-12-02', '2009-09-29', '2009-09-15', '2009-06-26', '2009-04-23', '2009-01-21',
+			'2008-12-03', '2008-10-14', '2008-04-08',
+			'2007-10-26',
+			'2006-11-16', '2006-06-12',
+		),
+		'bezpecnostPhpAplikaci' => array(
+			'2011-09-16', '2011-09-05', '2011-04-29',
+			'2010-12-09', '2010-10-08', '2010-06-11', '2010-03-12', '2010-03-09',
+			'2009-12-08', '2009-09-17', '2009-06-08', '2009-03-12', '2009-03-10',
+			'2008-12-08', '2008-10-21', '2008-06-24', '2008-02-28', '2008-02-25',
+			'2007-10-29', '2007-10-23', '2007-06-26', '2007-04-16',
+			'2006-10-27', '2006-06-22', '2006-04-25',
+		),
+		'vykonnostWebovychAplikaci' => array(
+			'2011-09-14', '2011-04-27',
+			'2010-12-07', '2010-03-10',
+			'2009-09-21', '2009-03-11',
+			'2008-12-09', '2008-10-22', '2008-06-27',
+		),
+	);
 
 
 	public function renderDefault()
 	{
 		$this->template->pageTitle = 'Školení';
 
-		$upcomingTrainings = $this->context->createTrainingDates()->where('end > NOW()')->order('key_training ASC');
-		$this->template->upcomingTrainings = $upcomingTrainings;
+		$this->template->upcomingTrainings = $this->context->createTrainingDates()
+			->where('end > NOW()')
+			->order('key_training ASC');
 	}
 
 
 	public function actionSkoleni($name)
 	{
-		$training = $this->context->createTrainingDates()->where('training.action', $name)->where('end > NOW()')->limit(1)->fetch();
+		$training = $this->context->createTrainingDates()
+			->where('training.action', $name)
+			->where('end > NOW()')
+			->limit(1)
+			->fetch();
+
+		$this->tentative = (boolean)$training->tentative;
 
 		$this->template->trainingId       = $training->id_date;
 		$this->template->pageTitle        = 'Školení ' . $training->training->name;
@@ -39,7 +84,7 @@ class SkoleniPresenter extends BasePresenter
 		$this->template->audience         = $training->training->audience;
 		$this->template->start            = $training->start;
 		$this->template->end              = $training->end;
-		$this->template->tentative        = $training->tentative;
+		$this->template->tentative        = $this->tentative;
 		$this->template->originalUrl      = $training->training->original_url;
 		$this->template->capacity         = $training->training->capacity;
 		$this->template->services         = $training->training->services;
@@ -50,7 +95,14 @@ class SkoleniPresenter extends BasePresenter
 		$this->template->venueName        = $training->venue->name;
 		$this->template->venueAddress     = $training->venue->address;
 		$this->template->venueDescription = $training->venue->description;
-		$this->template->pastTrainings    = $this->trainings[3]['pastTrainings']; // TODO fuck this off
+
+		$this->template->pastTrainingsMe = $this->context->createTrainingDates()
+			->where('training.action', $name)
+			->where('end < NOW()')
+			->order('start DESC')
+			->fetchPairs('id_date', 'start');
+
+		$this->template->pastTrainingsJakub = $this->pastTrainingsVrana[$name];
 	}
 
 
@@ -70,7 +122,7 @@ class SkoleniPresenter extends BasePresenter
 			->addRule(Form::EMAIL, 'Zadejte platnou e-mailovou adresu')
 			->addRule(Form::MAX_LENGTH, 'Maximální délka e-mailu je sto znaků', 100);
 
-		if (isset($this->trainings[$this->trainingId]['date'])) {
+		if (!$this->tentative) {
 			$form->addGroup('Fakturační údaje');
 			$form->addText('company', 'Obchodní jméno:')
 				->addCondition(Form::FILLED)
@@ -141,7 +193,7 @@ class SkoleniPresenter extends BasePresenter
 				));
 				$this->flashMessage('Díky, přihláška odeslána! Potvrzení společně s fakturou vám přijde do druhého pracovního dne.');
 			}
-			$action = (isset($this->trainings[$values['trainingId']]) ? $this->trainings[$values['trainingId']]['action'] : '');
+			$action = $form->parent->params['name'];
 			$this->redirect($this->getName() . ':' . $action);
 		} catch (PDOException $e) {
  			Debugger::log($e, Debugger::ERROR);
