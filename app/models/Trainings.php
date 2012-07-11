d043d7960e93aa3ce0f82ecd8b8d0b7e36db004e
Michal Špaček <mail@michalspacek.cz>
2012-07-11 23:31:59 +0000

    NFR

--- app/models/Trainings.php
+++ app/models/Trainings.php
@@ -13,7 +13,8 @@ class Trainings extends BaseModel
 
 	public function getUpcoming()
 	{
-		return $this->database->fetchAll('SELECT
+		return $this->database->fetchAll(
+			'SELECT
 				t.action,
 				t.name,
 				d.tentative,
@@ -29,27 +30,28 @@ class Trainings extends BaseModel
 
 	public function get($name)
 	{
-		return $this->database->fetch('SELECT
-			t.action,
-			d.id_date AS dateId,
-			t.name,
-			t.description,
-			t.content,
-			t.prerequisites,
-			t.audience,
-			d.start,
-			d.end,
-			d.tentative,
-			t.original_href AS originalHref,
-			t.capacity,
-			t.services,
-			t.price,
-			t.student_discount AS studentDiscount,
-			t.materials,
-			v.href AS venueHref,
-			v.name AS venueName,
-			v.address AS venueAddress,
-			v.description AS venueDescription
+		return $this->database->fetch(
+			'SELECT
+				t.action,
+				d.id_date AS dateId,
+				t.name,
+				t.description,
+				t.content,
+				t.prerequisites,
+				t.audience,
+				d.start,
+				d.end,
+				d.tentative,
+				t.original_href AS originalHref,
+				t.capacity,
+				t.services,
+				t.price,
+				t.student_discount AS studentDiscount,
+				t.materials,
+				v.href AS venueHref,
+				v.name AS venueName,
+				v.address AS venueAddress,
+				v.description AS venueDescription
 			FROM training_dates d
 				JOIN trainings t ON d.key_training = t.id_training
 				JOIN training_venues v ON d.key_venue = v.id_venue
@@ -63,9 +65,10 @@ class Trainings extends BaseModel
 
 	public function getPastTrainings($name)
 	{
-		return $this->database->fetchPairs('SELECT
-			d.id_date AS dateId,
-			d.start
+		return $this->database->fetchPairs(
+			'SELECT
+				d.id_date AS dateId,
+				d.start
 			FROM training_dates d
 				JOIN trainings t ON d.key_training = t.id_training
 			WHERE t.action = ?
