56a2e098e15f2f655e8bed42ccd0a170f3ca5e7d
Michal Špaček <mail@michalspacek.cz>
2013-06-11 23:52:41 +0000

    Manually adding a dude is now possible.

---
+++ site/app/modules/AdminModule/presenters/UcastniciPresenter.php
@@ -0,0 +1,167 @@
+<?php
+namespace AdminModule;
+
+use \MichalSpacekCz\Trainings;
+use \Nette\Application\UI\Form;
+
+/**
+ * Ucastnici presenter.
+ *
+ * @author     Michal Špaček
+ * @package    michalspacek.cz
+ */
+class UcastniciPresenter extends \BasePresenter
+{
+
+	/** @var array */
+	private $dates;
+
+
+	public function actionNovy()
+	{
+		$this->template->pageTitle = 'Nový účastník';
+	}
+
+
+	public function renderDefault()
+	{
+		$this->template->pageTitle = 'Import';
+	}
+
+
+	protected function createComponentApplication($formName)
+	{
+		$helpers = new \Bare\Next\Templating\Helpers();
+
+		$this->dates = $this->trainings->getUpcoming();
+		foreach ($this->dates as $training) {
+			foreach ($training->dates as $date) {
+				if ($date->tentative) {
+					$start = $helpers->localDate($date->start, 'cs', '%B %Y');
+				} else {
+					$start = \Nette\Templating\Helpers::date($date->start, 'j. n. Y');
+				}
+				$dates[$date->dateId] = "{$start} {$date->venueCity}" . ($date->tentative ? ' (předběžný termín)' : '') . " - {$training->name}";
+			}
+		}
+
+		$sources = array();
+		foreach ($this->trainings->getTrainingApplicationSources() as $source) {
+			$sources[$source->alias] = $source->name;
+		}
+
+		$statuses = array();
+		foreach ($this->trainings->getInitialStatuses() as $status) {
+			$statuses[$status] = $status;
+		}
+
+		$session = $this->getSession('application');
+
+		$form = new Form($this, $formName);
+		$form->addSelect('trainingId', 'Termín školení:', $dates)
+			->setDefaultValue($session->trainingId)
+			->setRequired('Vyberte prosím termín a místo školení')
+			->setPrompt('- vyberte termín a místo -');
+
+		$form->addGroup('Účastník');
+		$form->addText('name', 'Jméno a příjmení:')
+			->setRequired('Zadejte prosím jméno a příjmení')
+			->addRule(Form::MIN_LENGTH, 'Minimální délka jména a příjmení je %d znaky', 3)
+			->addRule(Form::MAX_LENGTH, 'Maximální délka jména a příjmení je %d znaků', 200);
+		$form->addText('email', 'E-mail:')
+			->setRequired('Zadejte prosím e-mailovou adresu')
+			->addRule(Form::EMAIL, 'Zadejte platnou e-mailovou adresu')
+			->addRule(Form::MAX_LENGTH, 'Maximální délka e-mailu je %d znaků', 200);
+
+		$form->addGroup('Fakturační údaje');
+		$form->addText('company', 'Obchodní jméno:')
+			->addCondition(Form::FILLED)
+			->addRule(Form::MIN_LENGTH, 'Minimální délka obchodního jména je %d znaky', 3)
+			->addRule(Form::MAX_LENGTH, 'Maximální délka obchodního jména je %d znaků', 200);
+		$form->addText('street', 'Ulice a číslo:')
+			->addCondition(Form::FILLED)
+			->addRule(Form::MIN_LENGTH, 'Minimální délka ulice a čísla je %d znaky', 3)
+			->addRule(Form::MAX_LENGTH, 'Maximální délka ulice a čísla je %d znaků', 200);
+		$form->addText('city', 'Město:')
+			->addCondition(Form::FILLED)
+			->addRule(Form::MIN_LENGTH, 'Minimální délka města je %d znaky', 2)
+			->addRule(Form::MAX_LENGTH, 'Maximální délka města je %d znaků', 200);
+		$form->addText('zip', 'PSČ:')
+			->addCondition(Form::FILLED)
+			->addRule(Form::PATTERN, 'PSČ musí mít 5 číslic', '([0-9]\s*){5}')
+			->addRule(Form::MAX_LENGTH, 'Maximální délka PSČ je %d znaků', 200);
+		$form->addText('companyId', 'IČ:')
+			->addCondition(Form::FILLED)
+			->addRule(Form::MIN_LENGTH, 'Minimální délka IČ je %d znaky', 6)
+			->addRule(Form::MAX_LENGTH, 'Maximální délka IČ je %d znaků', 200);
+		$form->addText('companyTaxId', 'DIČ:')
+			->addCondition(Form::FILLED)
+			->addRule(Form::MIN_LENGTH, 'Minimální délka DIČ je %d znaky', 6)
+			->addRule(Form::MAX_LENGTH, 'Maximální délka DIČ je %d znaků', 200);
+
+		$form->setCurrentGroup(null);
+		$form->addText('note', 'Poznámka:')
+			->addCondition(Form::FILLED)
+			->addRule(Form::MAX_LENGTH, 'Maximální délka poznámky je %d znaků', 2000);
+		$form->addSelect('status', 'Status:', $statuses)
+			->setDefaultValue($session->status)
+			->setRequired('Vyberte status')
+			->setPrompt('- vyberte status -');
+		$form->addSelect('source', 'Zdroj:', $sources)
+			->setDefaultValue($session->source)
+			->setRequired('Vyberte zdroj')
+			->setPrompt('- vyberte zdroj -');
+
+		$form->addSubmit('signUp', 'Odeslat');
+		$form->onSuccess[] = new \Nette\Callback($this, 'submittedApplication');
+
+		return $form;
+	}
+
+
+	private function findDate($dateId)
+	{
+		foreach ($this->dates as $training) {
+			foreach ($training->dates as $date) {
+				if ($date->dateId == $dateId) {
+					return $date;
+				}
+			}
+		}
+		return false;
+	}
+
+
+	public function submittedApplication($form)
+	{
+		$session = $this->getSession('application');
+
+		$values = $form->getValues();
+		$date = $this->findDate($values->trainingId);
+		try {
+			$this->trainings->insertApplication(
+				$values->trainingId,
+				$values->name,
+				$values->email,
+				$values->company,
+				$values->street,
+				$values->city,
+				$values->zip,
+				$values->companyId,
+				$values->companyTaxId,
+				$values->note,
+				($date->tentative ? Trainings::STATUS_TENTATIVE : $values->status),
+				$values->source
+			);
+			$session->trainingId = $values->trainingId;
+			$session->status     = $values->status;
+			$session->source     = $values->source;
+			$this->redirect($this->getAction());
+		} catch (PDOException $e) {
+			Debugger::log($e, Debugger::ERROR);
+			$this->flashMessage('Ups, něco se rozbilo a přihlášku se nepodařilo odeslat, zkuste to za chvíli znovu.', 'error');
+		}
+	}
+
+
+}
