0c8eb71f4742e54f79ff422cc044a53344184153
Michal Špaček <mail@michalspacek.cz>
2013-09-29 00:28:41 +0000

    Remove the trailing slash from admin URL.

--- site/app/router/RouterFactory.php
+++ site/app/router/RouterFactory.php
@@ -18,7 +18,7 @@ class RouterFactory
 	{
 		Route::addStyle('name', 'action');  // let the name param be converted like the action param (foo-bar => fooBar)
 		$router = new RouteList();
-		$router[] = new Route('admin/<presenter>[/<action>][/<param>]', array('module' => 'Admin', 'presenter' => 'Homepage', 'action' => 'default'), Route::SECURED);
+		$router[] = new Route('admin[/<presenter>][/<action>][/<param>]', array('module' => 'Admin', 'presenter' => 'Homepage', 'action' => 'default'), Route::SECURED);
 		$router[] = new Route('rozhovory/<name>', 'Rozhovory:rozhovor');
 		$router[] = new Route('prednasky/<name>[/<slide>]', 'Prednasky:prednaska');
 		$router[] = new Route('soubory[/<action>]/<filename>', 'Soubory:soubor');
