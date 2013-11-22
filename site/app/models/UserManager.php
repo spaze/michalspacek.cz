c12dbe454c71aeb10eb180dbd8bbbb025e913d39
Michal Špaček <mail@michalspacek.cz>
2013-11-22 02:46:39 +0100

    Introduce AES-256 encryption to the password storage

--- site/app/models/UserManager.php
+++ site/app/models/UserManager.php
@@ -12,6 +12,14 @@ class UserManager extends BaseModel implements \Nette\Security\IAuthenticator
 
 	const KNOCK_KNOCK = 'knockKnock';
 
+	protected $key;
+
+
+	public function setKey($key)
+	{
+		$this->key = $key;
+	}
+
 
 	public function verifySignInAuthorization($knockKnock)
 	{
@@ -52,23 +60,53 @@ class UserManager extends BaseModel implements \Nette\Security\IAuthenticator
 		if (!$user) {
 			throw new \Nette\Security\AuthenticationException('The username is incorrect.', self::IDENTITY_NOT_FOUND);
 		}
-		if ($user->password !== $this->calculateHash($password, $user->password)) {
+		if (!$this->verifyHash($password, $this->decryptPassword($user->password))) {
 			throw new \Nette\Security\AuthenticationException('The password is incorrect.', self::INVALID_CREDENTIAL);
 		}
 		return $user;
 	}
 
 
-	public function calculateHash($password, $salt = null)
+	private function calculateHash($password, $salt = null)
 	{
 		return crypt($password, $salt ?: '$2y$07$' . \Nette\Utils\Strings::random(22));
+	}
+
+	private function verifyHash($password, $hash)
+	{
+		return ($hash === $this->calculateHash($password, $hash));
+	}
+
+	private function getIvSize()
+	{
+		return $ivSize = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+	}
+
+
+	protected function encryptPassword($password)
+	{
+		$key = pack('H*', $this->key);
+		$iv = mcrypt_create_iv($this->getIvSize(), MCRYPT_RAND);
+		$encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $password, MCRYPT_MODE_CBC, $iv);
+		return base64_encode($iv . $encrypted);
+	}
+
+
+	protected function decryptPassword($password)
+	{
+		$encrypted = base64_decode($password);
+		$key = pack('H*', $this->key);
+		$iv = substr($encrypted, 0, $this->getIvSize());
+		$encrypted = substr($encrypted, $this->getIvSize());
+		$decrypted = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, $encrypted, MCRYPT_MODE_CBC, $iv);
+		return rtrim($decrypted, "\0");
 	}
 
 
 	public function changePassword($username, $password, $newPassword)
 	{
 		$user = $this->verifyPassword($username, $password);
-		$this->database->query('UPDATE users SET password = ? WHERE id_user = ?', $this->calculateHash($newPassword), $user->userId);
+		$this->database->query('UPDATE users SET password = ? WHERE id_user = ?', $this->encryptPassword($this->calculateHash($newPassword)), $user->userId);
 	}
 
 
