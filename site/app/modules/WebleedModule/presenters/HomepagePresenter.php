16f25e2d90137b04c81b0a1486d563ce5f60f321
Michal Špaček <mail@michalspacek.cz>
2014-04-30 20:27:39 +0200

    Jokes++

--- site/app/modules/WebleedModule/presenters/HomepagePresenter.php
+++ site/app/modules/WebleedModule/presenters/HomepagePresenter.php
@@ -43,6 +43,20 @@ class HomepagePresenter extends \BasePresenter
 		$this->template->points = $points;
 		$this->template->vulnerable = $vulnerable;
 		$this->template->daysSince = $daysSince->format('%a');
+		$this->template->smallPrint = $this->getSmallPrint();
+	}
+
+
+	private function getSmallPrint()
+	{
+		$smallPrint = array(
+			'Knocking on yer servar\'s ports since 2014.',
+			'Do you even scan?',
+			'Wow. So heart. Much bleed.',
+			'<script>alert(\'XSS\');</script>',
+			\Nette\Utils\Html::el()->setHtml('<a href="https://www.youtube.com/watch?v=BROWqjuTM0g">admin</a>'),
+		);
+		return $smallPrint[array_rand($smallPrint)];
 	}
 
 
