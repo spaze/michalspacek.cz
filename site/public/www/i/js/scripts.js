3bc5593d833cde0626f8e0c5c0b0d72761d2d310
Michal Špaček <mail@michalspacek.cz>
2014-07-09 03:52:47 +0200

    Fix JS (#frmapplication is now #frm-application) after update to Nette 2.2

--- site/public/www/i/js/scripts.js
+++ site/public/www/i/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('#termin a[href="#prihlaska"]').click(function() {
-		$('#frmapplication-trainingId').val($(this).data('id'));
+		$('#frm-application-trainingId').val($(this).data('id'));
 	});
 	$('#uskutecnene-terminy a[href="#uskutecnene-terminy"]').click(function() {
 		$('#uskutecnene-terminy-container').fadeToggle('fast');
