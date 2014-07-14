4f677608a2075884a32ae617763536d50c255609
Michal Špaček <mail@michalspacek.cz>
2014-07-14 22:07:03 +0200

    Kill exception when template helpers are not initialized and send HTTP 403

--- site/app/presenters/ForbiddenPresenter.php
+++ site/app/presenters/ForbiddenPresenter.php
@@ -10,14 +10,45 @@
 class ForbiddenPresenter extends \Nette\Application\UI\Presenter
 {
 
+	/** @var \Nette\Localization\ITranslator */
+	protected $translator;
+
+	/** @var \Nette\Http\IResponse */
+	protected $httpResponse;
+
 
 	/**
 	 * @param \Nette\Localization\ITranslator $translator
 	 */
-	public function __construct(\Nette\Localization\ITranslator $translator)
+	public function __construct(\Nette\Localization\ITranslator $translator, \Nette\Http\IResponse $httpResponse)
 	{
-		parent::__construct($translator);
+		$this->translator = $translator;
+		$this->httpResponse = $httpResponse;
 	}
 
+
+	public function beforeRender()
+	{
+		$webTracking = $this->getContext()->getByType(\MichalSpacekCz\WebTracking::class);
+		$this->template->trackingCode = $webTracking->isEnabled();
+		$this->template->setTranslator($this->translator);
+	}
+
+
+	protected function createTemplate($class = null)
+	{
+		$helpers = $this->getContext()->getByType(\MichalSpacekCz\Templating\Helpers::class);
+
+		$template = parent::createTemplate($class);
+		$template->getLatte()->addFilter(null, [new \Bare\Next\Templating\Helpers(), 'loader']);
+		$template->getLatte()->addFilter(null, [$helpers, 'loader']);
+		return $template;
+	}
+
+
+	public function actionDefault()
+	{
+		$this->httpResponse->setCode(\Nette\Http\Response::S403_FORBIDDEN);
+	}
 
 }
