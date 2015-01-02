67c086fa60060cc5381399a701b728d24d82027b
Michal Špaček <mail@michalspacek.cz>
2015-01-02 06:30:39 +0100

    Load company details using AJAX

--- site/public/www/i/js/scripts.js
+++ site/public/www/i/js/scripts.js
@@ -7,4 +7,44 @@ $(document).ready(function() {
 		return false;
 	});
 	$('#uskutecnene-terminy-container').hide();
+
+	var Application = {};
+	Application.loadDataControls = function() {
+		$('#loadDataControls > span').hide();
+		$('#loadDataAgain').show();
+		$('#loadDataAgain a').click(Application.loadData);
+	};
+	Application.loadData = function(event) {
+		$('#loadDataControls > span').hide();
+		$('#loadDataWait').show();
+		event.preventDefault();
+		var load = $.ajax({
+			url: $('#loadData').data('url'),
+			data: {
+				country: $('#frm-application-country').val(),
+				companyId: $('#frm-application-companyId').val().replace(/ /g, ''),
+			},
+			timeout: 5000
+		});
+		load.done(function(data) {
+			if (data.status == 200) {
+				$.each(['companyId', 'companyTaxId', 'company', 'street', 'city', 'zip', 'country'], function(key, value) {
+					$('#frm-application-' + value).val(data[value]);
+				});
+				Application.loadDataControls();
+			} else if (data.status == 400) {
+				Application.loadDataControls();
+				$('#loadDataNotFound').show();
+			} else {
+				Application.loadDataControls();
+				$('#loadDataError').show();
+			}
+		});
+		load.fail(function() {
+			Application.loadDataControls();
+			$('#loadDataError').show();
+		});
+	};
+	$('#loadData').click(Application.loadData);
+	$('#loadData').show();
 });
