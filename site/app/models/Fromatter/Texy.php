ed2e0a06269c2451e113ac891b52139da89b52cb
Michal Špaček <mail@michalspacek.cz>
2015-01-07 18:33:47 +0100

    Translation strings can be Texy-formatted and support placeholders for links

---
+++ site/app/models/Fromatter/Texy.php
@@ -0,0 +1,78 @@
+<?php
+namespace MichalSpacekCz\Formatter;
+
+class Texy extends \Bare\Next\Formatter\Texy
+{
+
+	/** @var \Nette\Localization\ITranslator */
+	protected $translator;
+
+	/** @var \Nette\Application\Application */
+	protected $application;
+
+
+	public function __construct(
+		\Nette\Caching\IStorage $cacheStorage,
+		\Nette\Localization\ITranslator $translator,
+		\Nette\Application\Application $application
+	)
+	{
+		$this->translator = $translator;
+		$this->application = $application;
+		parent::__construct($cacheStorage);
+	}
+
+
+	/**
+	 * @param string $format
+	 * @param string $args
+	 * @return \Nette\Utils\Html
+	 */
+	public function substitute($format, $args)
+	{
+		return parent::format(vsprintf($format, $args));
+	}
+
+
+	/**
+	 * @param string $message
+	 * @param array $replacements
+	 * @return string
+	 */
+	public function translate($message, array $replacements = null)
+	{
+		return $this->substitute($this->translator->translate($message), $replacements);
+	}
+
+
+	/**
+	 * @param \Texy $texy
+	 */
+	protected function addHandlers(\Texy $texy)
+	{
+		$texy->addHandler('phrase', [$this, 'phraseHandler']);
+	}
+
+
+	/**
+	 * @param TexyHandlerInvocation  handler invocation
+	 * @param string
+	 * @param string
+	 * @param TexyModifier
+	 * @param TexyLink
+	 * @return TexyHtml|string|FALSE
+	 */
+	function phraseHandler($invocation, $phrase, $content, $modifier, $link)
+	{
+		if (!$link) {
+			return $invocation->proceed();
+		}
+
+		if (strncmp($link->URL, 'link:', 5) === 0) {
+			$link->URL = $this->application->getPresenter()->link(substr($link->URL, 5));
+		}
+
+		return $invocation->proceed();
+	}
+
+}
--- site/app/models/Templating/Helpers.php
+++ site/app/models/Templating/Helpers.php
@@ -4,8 +4,17 @@ namespace MichalSpacekCz\Templating;
 class Helpers extends \Nette\Object
 {
 
+	/** @var \MichalSpacekCz\Formatter\Texy */
+	protected $texyFormatter;
+
 	/** @var string */
 	protected $staticRoot;
+
+
+	public function __construct(\MichalSpacekCz\Formatter\Texy $texyFormatter)
+	{
+		$this->texyFormatter = $texyFormatter;
+	}
 
 
 	public function loader($helper)
@@ -30,6 +39,14 @@ class Helpers extends \Nette\Object
 	public function setStaticRoot($staticRoot)
 	{
 		$this->staticRoot = $staticRoot;
+	}
+
+
+	public function format($message)
+	{
+		$args = func_get_args();
+		array_shift($args);
+		return $this->texyFormatter->substitute($message, $args);
 	}
 
 }
