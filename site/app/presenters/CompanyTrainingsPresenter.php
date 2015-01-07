eaba234698cd1738d150e9f9130953d2cc26f09b
Michal Špaček <mail@michalspacek.cz>
2015-01-07 18:34:19 +0100

    Translated CompanyTrainings

--- site/app/presenters/CompanyTrainingsPresenter.php
+++ site/app/presenters/CompanyTrainingsPresenter.php
@@ -8,6 +8,9 @@
 class CompanyTrainingsPresenter extends BasePresenter
 {
 
+	/** @var \MichalSpacekCz\Formatter\Texy */
+	protected $texyFormatter;
+
 	/** @var \MichalSpacekCz\Training\Trainings */
 	protected $trainings;
 
@@ -17,15 +20,18 @@ class CompanyTrainingsPresenter extends BasePresenter
 
 	/**
 	 * @param \Nette\Localization\ITranslator $translator
+	 * @param \MichalSpacekCz\Formatter\Texy $texyFormatter
 	 * @param \MichalSpacekCz\Training\Trainings $trainings
 	 * @param \MichalSpacekCz\Vat $vat
 	 */
 	public function __construct(
 		\Nette\Localization\ITranslator $translator,
+		\MichalSpacekCz\Formatter\Texy $texyFormatter,
 		\MichalSpacekCz\Training\Trainings $trainings,
 		\MichalSpacekCz\Vat $vat
 	)
 	{
+		$this->texyFormatter = $texyFormatter;
 		$this->trainings = $trainings;
 		$this->vat = $vat;
 		parent::__construct($translator);
@@ -34,7 +40,7 @@ class CompanyTrainingsPresenter extends BasePresenter
 
 	public function renderDefault()
 	{
-		$this->template->pageTitle = 'Školení pro firmy';
+		$this->template->pageTitle = $this->translator->translate('messages.title.companytrainings');
 		$this->template->trainings = $this->trainings->getNames();
 	}
 
@@ -46,7 +52,7 @@ class CompanyTrainingsPresenter extends BasePresenter
 			throw new \Nette\Application\BadRequestException("I don't do {$name} training, yet", Response::S404_NOT_FOUND);
 		}
 		$this->template->name = $training->action;
-		$this->template->pageTitle = 'Firemní školení ' . $training->name;
+		$this->template->pageTitle = $this->texyFormatter->translate('messages.title.companytraining', [$training->name]);
 		$this->template->title = $training->name;
 		$this->template->descriptionCompany = $training->descriptionCompany;
 		$this->template->content = $training->content;
