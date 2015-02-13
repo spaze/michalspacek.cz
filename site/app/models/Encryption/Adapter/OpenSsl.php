2a5e1d645dc217bb1138952971fc81b969ac36e2
Michal Špaček <mail@michalspacek.cz>
2015-02-13 05:20:28 +0100

    Add OpenSSL encryption adapter, not used for now

---
+++ site/app/models/Encryption/Adapter/OpenSsl.php
@@ -0,0 +1,49 @@
+<?php
+namespace MichalSpacekCz\Encryption\Adapter;
+
+/**
+ * OpenSSL encryption adapter.
+ *
+ * @author     Michal Špaček
+ * @package    michalspacek.cz
+ */
+class OpenSSL implements \MichalSpacekCz\Encryption\Adapter\AdapterInterface
+{
+
+	public function encrypt($clearText, $key, $cipher, $iv)
+	{
+		return openssl_encrypt($clearText, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+	}
+
+
+	public function decrypt($cipherText, $key, $cipher, $iv)
+	{
+		return openssl_decrypt($cipherText, $cipher, $key, OPENSSL_RAW_DATA, $iv);
+	}
+
+
+	public function getIvLength($cipher)
+	{
+		return openssl_cipher_iv_length($cipher);
+	}
+
+
+	public function createIv($length)
+	{
+		$random = false;
+		$strong = false;
+
+		$i = 0;
+		while (!$strong && $i < 10) {
+			$random = openssl_random_pseudo_bytes($length, $strong);
+			$i++;
+		}
+
+		if ($random === false) {
+			throw new \RuntimeException("Error creating IV, tried $i times");
+		}
+
+		return $random;
+	}
+
+}
