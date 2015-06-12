e7d70a8fc031e5327b2ac2b4e3499ba4e2eaaa03
Michal Špaček <mail@michalspacek.cz>
2015-06-12 15:28:39 +0200

    Do host detection in SecurityHeaders even for CSP and also send headers on naked domain

--- site/app/models/ContentSecurityPolicy.php
+++ site/app/models/ContentSecurityPolicy.php
@@ -10,40 +10,20 @@ namespace MichalSpacekCz;
 class ContentSecurityPolicy
 {
 
-	/** @var string */
-	protected $rootDomain;
-
 	/** @var array of host => array of policies */
 	protected $policy = array();
-
-	/** @var \Nette\Http\IRequest */
-	protected $httpRequest;
-
-
-	public function __construct(\Nette\Http\IRequest $httpRequest)
-	{
-		$this->httpRequest = $httpRequest;
-	}
-
-
-	public function setRootDomain($rootDomain)
-	{
-		$this->rootDomain = $rootDomain;
-	}
 
 
 	public function setPolicy($policy)
 	{
 		foreach ($policy as $host => $sources) {
-			$this->policy["{$host}.{$this->rootDomain}"] = $sources;
+			$this->policy[$host] = $sources;
 		}
 	}
 
 
-	public function getHeader()
+	public function getHeader($host)
 	{
-		$host = $this->httpRequest->getUrl()->getHost();
-
 		if (!isset($this->policy[$host])) {
 			return false;
 		}
--- site/app/models/SecurityHeaders.php
+++ site/app/models/SecurityHeaders.php
@@ -11,6 +11,9 @@ class SecurityHeaders
 {
 
 	/** @var string */
+	protected $defaultDomain;
+
+	/** @var string */
 	protected $rootDomain;
 
 	/** @var array of host => headers */
@@ -43,6 +46,12 @@ class SecurityHeaders
 	}
 
 
+	public function setDefaultDomain($defaultDomain)
+	{
+		$this->defaultDomain = $defaultDomain;
+	}
+
+
 	public function setRootDomain($rootDomain)
 	{
 		$this->rootDomain = $rootDomain;
@@ -52,23 +61,27 @@ class SecurityHeaders
 	public function setExtraHeaders($extraHeaders)
 	{
 		foreach ($extraHeaders as $host => $headers) {
-			$this->extraHeaders["{$host}.{$this->rootDomain}"] = $headers;
+			$this->extraHeaders[$host] = $headers;
 		}
 	}
 
 
 	public function sendHeaders()
 	{
+		if ($this->httpRequest->getUrl()->getHost() === $this->rootDomain) {
+			$host = $this->defaultDomain;
+		} else {
+			$host = str_replace(".{$this->rootDomain}", '', $this->httpRequest->getUrl()->getHost());
+		}
+
 		if ($this->httpRequest->isSecured()) {
 			$this->httpResponse->setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload');
 		}
 
-		$header = $this->contentSecurityPolicy->getHeader();
+		$header = $this->contentSecurityPolicy->getHeader($host);
 		if ($header !== false) {
 			$this->httpResponse->setHeader('Content-Security-Policy', $header);
 		}
-
-		$host = $this->httpRequest->getUrl()->getHost();
 
 		if (isset($this->extraHeaders[$host])) {
 			foreach ($this->extraHeaders[$host] as $name => $value) {
