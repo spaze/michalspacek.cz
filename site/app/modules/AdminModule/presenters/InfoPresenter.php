44fe6a527dab657f98bb8345883673a37d7a4b80
Michal Špaček <mail@michalspacek.cz>
2015-09-25 00:21:14 +0200

    CSP: Convert phpinfo's inline styles to classes so we can drop style-src 'unsafe-inline' for admin
    See c25d8b58a516da84ec3509076fb30949e6a829f4

--- site/app/modules/AdminModule/presenters/InfoPresenter.php
+++ site/app/modules/AdminModule/presenters/InfoPresenter.php
@@ -15,6 +15,8 @@ class InfoPresenter extends BasePresenter
 		ob_start();
 		phpinfo();
 		$info = preg_replace('~^.*?(<table[^>]*>.*</table>).*$~s', '$1', ob_get_clean());
+		// Convert inline styles to classes defined in admin/info.css so we can drop CSP style-src 'unsafe-inline'
+		$info = str_replace('style="color: #', 'class="color-', $info);
 
 		$this->template->pageTitle = 'phpinfo()';
 		$this->template->phpinfo = \Nette\Utils\Html::el()->setHtml($info);
