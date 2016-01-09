65a37d94dbbf8f0a684c0a427ee6fd9256633def
Michal Špaček <mail@michalspacek.cz>
2016-01-09 03:18:24 +0100

    Store back the alternative value

--- site/public/www/i/js/upckeys.js
+++ site/public/www/i/js/upckeys.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
 		s.data('alt', s.val());
 		s.val(alt).prop('disabled', true);
 		setTimeout(function() {
+			var alt = s.val();
 			s.val(s.data('alt')).prop('disabled', false);
+			s.data('alt', alt);
 		}, 5000);
 	});
 });
