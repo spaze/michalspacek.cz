c60a8c3085aa26b1fce7e766112db03790d21fe5
Michal Špaček <mail@michalspacek.cz>
2016-01-09 07:49:49 +0100

    When submitting form make sure it was not submitted already

--- site/public/www/i/js/upckeys.js
+++ site/public/www/i/js/upckeys.js
@@ -1,5 +1,10 @@
 $(document).ready(function() {
+	var submitted = false;
 	$('#frm-ssid').submit(function() {
+		if (submitted) {
+			return false;
+		}
+		submitted = true;
 		var s = $('#submit');
 		var alt = s.data('alt');
 		s.data('alt', s.val());
@@ -8,6 +13,7 @@ $(document).ready(function() {
 			var alt = s.val();
 			s.val(s.data('alt')).prop('disabled', false);
 			s.data('alt', alt);
+			submitted = false;
 		}, 5000);
 	});
 });
