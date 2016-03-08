106f31b1a06112749092107975a4cd3decd8f77f
Michal Špaček <mail@michalspacek.cz>
2016-03-08 01:02:44 +0100

    Add my PGP key directly to the contact page

--- site/app/presenters/ContactPresenter.php
+++ site/app/presenters/ContactPresenter.php
@@ -14,6 +14,8 @@ class ContactPresenter extends BasePresenter
 	public function renderDefault()
 	{
 		$this->template->pageTitle  = $this->translator->translate('messages.title.contact');
+		$this->template->keyFile = $keyFile = 'key.asc';
+		$this->template->key = file_get_contents($keyFile);
 	}
 
 
