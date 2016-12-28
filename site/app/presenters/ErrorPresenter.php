e9bedf0b776a7f06f4305d552f084d30950fa4bd
Michal Špaček <mail@michalspacek.cz>
2016-12-29 00:15:20 +0100

    Change 4xx to 400 because that's an integer and goes well with other codes (and strict typing)

--- site/app/presenters/ErrorPresenter.php
+++ site/app/presenters/ErrorPresenter.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Presenters;
 
+use \Nette\Http\IResponse;
+
 /**
  * Error presenter.
  *
@@ -12,6 +14,15 @@ class ErrorPresenter extends BasePresenter
 
 	/** @var \MichalSpacekCz\Redirections */
 	protected $redirections;
+
+	/** @var array */
+	protected $statuses = [
+		IResponse::S400_BAD_REQUEST,
+		IResponse::S403_FORBIDDEN,
+		IResponse::S404_NOT_FOUND,
+		IResponse::S405_METHOD_NOT_ALLOWED,
+		IResponse::S410_GONE,
+	];
 
 
 	/**
@@ -39,12 +50,10 @@ class ErrorPresenter extends BasePresenter
 	{
 		$destination = $this->redirections->getDestination($this->getHttpRequest()->getUrl());
 		if ($destination) {
-			$this->redirectUrl($destination, \Nette\Http\IResponse::S301_MOVED_PERMANENTLY);
+			$this->redirectUrl($destination, IResponse::S301_MOVED_PERMANENTLY);
 		}
 
-		$code = $exception->getCode();
-		$code = (in_array($code, [403, 404, 405, 410]) ? $code : '4xx');
-
+		$code = (in_array($exception->getCode(), $this->statuses) ? $exception->getCode() : IResponse::S400_BAD_REQUEST);
 		$this->template->errorCode = $code;
 		$this->template->pageTitle = $this->translator->translate("messages.title.error{$code}");
 		$this->template->note =  $this->translator->translate("messages.error.{$code}");
