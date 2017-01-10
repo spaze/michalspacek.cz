352b837191fa4ea704fd74a8b429e59c234fd588
Michal Špaček <mail@michalspacek.cz>
2017-01-10 06:38:01 +0100

    Another shot at blog, now with sidebar with training dates

--- site/app/modules/Blog/presenters/PostPresenter.php
+++ site/app/modules/Blog/presenters/PostPresenter.php
@@ -15,13 +15,18 @@ class PostPresenter extends \App\WwwModule\Presenters\BasePresenter
 	/** @var \MichalSpacekCz\Blog\Post */
 	protected $blogPost;
 
+	/** @var \MichalSpacekCz\Training\Dates */
+	protected $trainingDates;
+
 
 	/**
 	 * @param \MichalSpacekCz\Blog\Post $blogPost
+	 * @param \MichalSpacekCz\Training\Dates $trainingDates
 	 */
-	public function __construct(\MichalSpacekCz\Blog\Post $blogPost)
+	public function __construct(\MichalSpacekCz\Blog\Post $blogPost, \MichalSpacekCz\Training\Dates $trainingDates)
 	{
 		$this->blogPost = $blogPost;
+		$this->trainingDates = $trainingDates;
 		parent::__construct();
 	}
 
@@ -31,6 +36,7 @@ class PostPresenter extends \App\WwwModule\Presenters\BasePresenter
 		$post = $this->blogPost->get($post);
 		$this->template->post = $post;
 		$this->template->pageTitle = strip_tags((string)$post->title);
+		$this->template->upcomingTrainings = $this->trainingDates->getPublicUpcoming();
 	}
 
 }
