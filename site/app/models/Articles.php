521eadc080ca37558830f9dea4082a1a2558f8d6
Michal Špaček <mail@michalspacek.cz>
2017-01-19 17:31:35 +0100

    Merge blog posts with "3rd party" articles

--- site/app/models/Articles.php
+++ site/app/models/Articles.php
@@ -18,11 +18,25 @@ class Articles
 	/** @var \Netxten\Formatter\Texy */
 	protected $texyFormatter;
 
+	/** @var \Nette\Application\LinkGenerator */
+	protected $linkGenerator;
 
-	public function __construct(\Nette\Database\Context $context, \Netxten\Formatter\Texy $texyFormatter)
+	/** @var \Nette\Localization\ITranslator */
+	protected $translator;
+
+
+	/**
+	 * @param \Nette\Database\Context $context
+	 * @param \Netxten\Formatter\Texy $texyFormatter
+	 * @param \Nette\Application\LinkGenerator $linkGenerator
+	 * @param \Nette\Localization\ITranslator $translator
+	 */
+	public function __construct(\Nette\Database\Context $context, \Netxten\Formatter\Texy $texyFormatter, \Nette\Application\LinkGenerator $linkGenerator, \Nette\Localization\ITranslator $translator)
 	{
 		$this->database = $context;
 		$this->texyFormatter = $texyFormatter;
+		$this->linkGenerator = $linkGenerator;
+		$this->translator = $translator;
 	}
 
 
@@ -43,6 +57,15 @@ class Articles
 				s.href AS sourceHref
 			FROM articles a
 				JOIN article_sources s ON a.key_article_source = s.id_article_source
+			UNION ALL
+				SELECT
+					bp.title,
+					bp.slug,
+					bp.published,
+					bp.lead,
+					null,
+					null
+				FROM blog_posts bp
 			ORDER BY date DESC';
 
 		if ($limit !== null) {
@@ -50,7 +73,12 @@ class Articles
 		}
 
 		$articles = $this->database->fetchAll($query);
-		foreach ($articles as &$article) {
+		foreach ($articles as $article) {
+			if ($article->sourceHref === null) {
+				$article->href = $this->linkGenerator->link('Blog:Post:', [$article->href]);
+				$article->sourceName = $this->translator->translate('messages.title.blog');
+				$article->sourceHref = $this->linkGenerator->link('Www:Articles:');
+			}
 			$article->excerpt = $this->texyFormatter->format($article->excerpt);
 		}
 		return $articles;
