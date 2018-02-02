6c60f2a4fed6edc93733799ce678aa0dc22980eb
Michal Špaček <mail@michalspacek.cz>
2018-02-02 22:12:30 +0100

    Don't check if site already exists when adding a new site

--- site/app/modules/Admin/presenters/PulsePresenter.php
+++ site/app/modules/Admin/presenters/PulsePresenter.php
@@ -88,7 +88,7 @@ class PulsePresenter extends BasePresenter
 			if ($values->site->id === Sites::ALL && !empty($specificSites)) {
 				$form->addError('Invalid combination, can\'t add disclosure for all sites when sites already exist');
 			}
-			if ($values->site->id !== Sites::ALL && !isset($storages->sites[$values->site->id])) {
+			if ($values->site->id !== null && $values->site->id !== Sites::ALL && !isset($storages->sites[$values->site->id])) {
 				$form->addError('Invalid combination, the site is already assigned to different company');
 			}
 		} elseif ($this->companies->getByName($values->company->new->name)) {
