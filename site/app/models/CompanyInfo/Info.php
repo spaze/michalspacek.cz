545fe71f609eb21a4bda68ce7153d898430bf01a
Michal Špaček <mail@michalspacek.cz>
2018-04-22 02:15:44 +0200

    Cache company info service results

--- site/app/models/CompanyInfo/Info.php
+++ site/app/models/CompanyInfo/Info.php
@@ -1,6 +1,8 @@
 <?php
 namespace MichalSpacekCz\CompanyInfo;
 
+use Nette\Caching\Cache;
+
 /**
  * Company info service.
  *
@@ -22,59 +24,68 @@ class Info
 	/** @var RegisterUz */
 	private $registerUz;
 
+	/** @var \Nette\Caching\Cache */
+	private $cache;
+
 	/** @var boolean */
 	private $loadCompanyDataVisible = true;
 
 
 	/**
 	 * @param Ares $ares
+	 * @param RegisterUz $registerUz
+	 * @param \Nette\Caching\IStorage $cacheStorage
 	 */
-	public function __construct(
-		Ares $ares,
-		RegisterUz $registerUz
-	)
+	public function __construct(Ares $ares, RegisterUz $registerUz, \Nette\Caching\IStorage $cacheStorage)
 	{
 		$this->ares = $ares;
 		$this->registerUz = $registerUz;
+		$this->cache = new Cache($cacheStorage, self::class);
 	}
 
 
 	public function getData($country, $companyId)
 	{
-		switch ($country) {
-			case 'cz':
-				$data = $this->ares->getData($companyId);
-				switch ($data->status) {
-					case Ares::STATUS_ERROR:
-						$data->status = self::STATUS_ERROR;
-						break;
-					case Ares::STATUS_FOUND:
-						$data->status = self::STATUS_FOUND;
-						break;
-					case Ares::STATUS_NOT_FOUND:
-						$data->status = self::STATUS_NOT_FOUND;
-						break;
-				}
-				break;
-			case 'sk':
-				$data = $this->registerUz->getData($companyId);
-				switch ($data->status) {
-					case Ares::STATUS_ERROR:
-						$data->status = self::STATUS_ERROR;
-						break;
-					case Ares::STATUS_FOUND:
-						$data->status = self::STATUS_FOUND;
-						break;
-					case Ares::STATUS_NOT_FOUND:
-						$data->status = self::STATUS_NOT_FOUND;
-						break;
-				}
-				break;
-			default:
-				throw new \RuntimeException('Unsupported country');
-				break;
-		}
-		return $data;
+		return $this->cache->load("{$country}/{$companyId}", function(&$dependencies) use ($country, $companyId) {
+			$found = false;
+			switch ($country) {
+				case 'cz':
+					$data = $this->ares->getData($companyId);
+					switch ($data->status) {
+						case Ares::STATUS_ERROR:
+							$data->status = self::STATUS_ERROR;
+							break;
+						case Ares::STATUS_FOUND:
+							$data->status = self::STATUS_FOUND;
+							$found = true;
+							break;
+						case Ares::STATUS_NOT_FOUND:
+							$data->status = self::STATUS_NOT_FOUND;
+							break;
+					}
+					break;
+				case 'sk':
+					$data = $this->registerUz->getData($companyId);
+					switch ($data->status) {
+						case Ares::STATUS_ERROR:
+							$data->status = self::STATUS_ERROR;
+							break;
+						case Ares::STATUS_FOUND:
+							$data->status = self::STATUS_FOUND;
+							$found = true;
+							break;
+						case Ares::STATUS_NOT_FOUND:
+							$data->status = self::STATUS_NOT_FOUND;
+							break;
+					}
+					break;
+				default:
+					throw new \RuntimeException('Unsupported country');
+					break;
+			}
+			$dependencies[Cache::EXPIRATION] = ($found ? '3 days' : '15 minutes');
+			return $data;
+		});
 	}
 
 
