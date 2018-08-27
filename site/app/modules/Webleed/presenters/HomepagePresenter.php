80142eb4e004b1b00dab6b19894743068cb883a3
Michal Špaček <mail@michalspacek.cz>
2018-08-27 02:37:11 +0200

    Replace Highcharts-powered chart with a static image
    The data is not updated anymore and maintaining the Highcharts lib became an unnecessary burden

--- site/app/modules/Webleed/presenters/HomepagePresenter.php
+++ site/app/modules/Webleed/presenters/HomepagePresenter.php
@@ -12,45 +12,10 @@ namespace App\WebleedModule\Presenters;
 class HomepagePresenter extends \App\WwwModule\Presenters\BasePresenter
 {
 
-	private const HEARTBLEED_DISCLOSURE = '2014-04-07';
-
-	/** @var \Nette\Database\Context */
-	protected $database;
-
-
-	/**
-	 * @param \Nette\Database\Context $context
-	 */
-	public function __construct(\Nette\Database\Context $context)
-	{
-		$this->database = $context;
-		parent::__construct();
-	}
-
-
 	public function actionDefault(): void
 	{
-		$points = array();
-		$vulnerable = null;
-		$disclosure = $latest = new \DateTime(self::HEARTBLEED_DISCLOSURE);
-		$result = $this->database->fetchAll('SELECT date, port, vulnerable, total FROM webleed ORDER BY date, port');
-		foreach ($result as $row) {
-			$points[$row->port ?: 'any'][] = \Nette\Utils\Json::encode(array(
-				(int)$row->date->format('Y'),
-				(int)$row->date->format('n') - 1,  // Dear JS...
-				(int)$row->date->format('j'),
-				$row->vulnerable,
-				round($row->vulnerable / $row->total * 100, 2),
-			));
-			if ($row->port === null) {
-				$vulnerable = $row->vulnerable;
-			}
-			$latest = $row->date;
-		}
-		$daysSince = $disclosure->diff($latest);
-		$this->template->points = $points;
-		$this->template->vulnerable = $vulnerable;
-		$this->template->daysSince = $daysSince->format('%a');
+		$this->template->vulnerable = 1543;
+		$this->template->daysSince = 371;
 		$this->template->smallPrint = $this->getSmallPrint();
 	}
 
@@ -61,9 +26,9 @@ class HomepagePresenter extends \App\WwwModule\Presenters\BasePresenter
 	private function getSmallPrint(): \Nette\Utils\Html
 	{
 		$smallPrint = array(
-			// 'Knocking on yer servar\'s ports since 2014.',
-			// 'Do you even scan?',
-			// 'Wow. So heart. Much bleed.',
+			 'Knocking on yer servar\'s ports since 2014.',
+			 'Do you even scan?',
+			 'Wow. So heart. Much bleed.',
 			htmlspecialchars('<script>alert(\'XSS\');</script>'),
 			'<a href="https://www.youtube.com/watch?v=DLzxrzFCyOs">admin</a>',
 		);
