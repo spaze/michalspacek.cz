c45a339a988475ce82e987517b55f65c53e413dc
Michal Špaček <mail@michalspacek.cz>
2018-11-08 15:49:38 +0100

    Wrap the code into an anonymous function to not pollute global space

--- site/public/www/i/js/remove-fbclid.js
+++ site/public/www/i/js/remove-fbclid.js
@@ -1,14 +1,16 @@
-var param = 'fbclid';
-if (location.search.indexOf(param + '=') !== -1) {
-	var replace = '';
-	try {
-		var url = new URL(location);
-		url.searchParams.delete(param);
-		replace = url.pathname + url.search + url.hash;
-	} catch (ex) {
-		var regExp = new RegExp('[?&]' + param + '=.*$');
-		replace = location.search.replace(regExp, '');
-		replace = location.pathname + replace + location.hash;
+(function() {
+	var param = 'fbclid';
+	if (location.search.indexOf(param + '=') !== -1) {
+		var replace = '';
+		try {
+			var url = new URL(location);
+			url.searchParams.delete(param);
+			replace = url.pathname + url.search + url.hash;
+		} catch (ex) {
+			var regExp = new RegExp('[?&]' + param + '=.*$');
+			replace = location.search.replace(regExp, '');
+			replace = location.pathname + replace + location.hash;
+		}
+		history.replaceState(null, '', replace);
 	}
-	history.replaceState(null, '', replace);
-}
+})();
