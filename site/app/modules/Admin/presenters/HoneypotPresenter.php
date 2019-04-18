3c606c779eb2012babcc03110bd43f01d9b58c59
Michal Špaček <mail@michalspacek.cz>
2019-04-18 15:39:35 +0200

    Make the error messages more informative

--- site/app/modules/Admin/presenters/HoneypotPresenter.php
+++ site/app/modules/Admin/presenters/HoneypotPresenter.php
@@ -4,6 +4,9 @@ declare(strict_types = 1);
 namespace App\AdminModule\Presenters;
 
 use MichalSpacekCz\Form\SignInHoneypot;
+use Nette\Utils\ArrayHash;
+use Nette\Utils\Html;
+use Tracy\Debugger;
 
 /**
  * Honeypot presenter.
@@ -28,10 +31,26 @@ class HoneypotPresenter extends \App\WwwModule\Presenters\BasePresenter
 	}
 
 
-	public function submittedSignIn(SignInHoneypot $form, \Nette\Utils\ArrayHash $values): void
+	public function submittedSignIn(SignInHoneypot $form, ArrayHash $values): void
 	{
-		\Tracy\Debugger::log("Sign-in attempt: {$values->username}, {$values->password}, {$this->getHttpRequest()->getRemoteAddress()}", 'honeypot');
-		$form->addError('Špatné uživatelské jméno nebo heslo');
+		Debugger::log("Sign-in attempt: {$values->username}, {$values->password}, {$this->getHttpRequest()->getRemoteAddress()}", 'honeypot');
+		$creds = $values->username . ':' . $values->password;
+		if (preg_match('~\slimit\s~i', $creds)) {
+			$message = Html::el()
+				->setText("No, no, no, no, no, no, no, no, no, no, no, no there's ")
+				->addHtml(Html::el('a')
+					->href('https://youtu.be/UKmsUAKWclE?t=8')
+					->setText('no ')->addHtml(Html::el('code')
+						->setText('limit')))
+				->addText('!');
+		} elseif (stripos($creds, 'honeypot') !== false) {
+			$message = 'Jo jo, honeypot, přesně tak';
+		} elseif (preg_match('~\sor\s~i', $creds)) {
+			$message = 'Dobrej pokusql!';
+		} else {
+			$message = 'Špatné uživatelské jméno nebo heslo';
+		}
+		$form->addError($message);
 	}
 
 }
