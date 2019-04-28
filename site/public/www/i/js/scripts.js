7304ecb5664091fdcc33f85a8080a39efe8ebecd
Michal Špaček <mail@michalspacek.cz>
2019-04-29 00:03:08 +0200

    PGP encryption is not supported on IE11, show a warning in the textarea

--- site/public/www/i/js/scripts.js
+++ site/public/www/i/js/scripts.js
@@ -53,6 +53,7 @@ $(document).ready(function() {
 	APPLICATION.changeLabels();
 
 	var ENCRYPTION = ENCRYPTION || {};
+	ENCRYPTION.supported = Boolean(window.Promise);
 	ENCRYPTION.feedback = $(document.queryCommandSupported('copy') ? '#copied' : '#copythis');
 	ENCRYPTION.button = $('#encrypt');
 	ENCRYPTION.area = $('#message');
@@ -85,38 +86,46 @@ $(document).ready(function() {
 				})[0]
 		);
 	};
-	ENCRYPTION.handler = async function() {
-		openpgp.config.commentstring = location.href;
-		options = {
-			message: openpgp.message.fromText(ENCRYPTION.area.val()),
-			publicKeys: (await openpgp.key.readArmored($('#pubkey').text())).keys,
-		};
-		openpgp.encrypt(options).then(function(ciphertext) {
-			ENCRYPTION.area.val(ciphertext.data);
-			ENCRYPTION.button.text(ENCRYPTION.button.data('copy'));
-			ENCRYPTION.button.off('click').click(function() {
-				ENCRYPTION.area.select();
-				if (document.queryCommandSupported('copy')) {
-					document.execCommand('copy');
-				}
-				ENCRYPTION.feedback.fadeIn('fast');
+	if (ENCRYPTION.supported) {
+		ENCRYPTION.handler = function() {
+			openpgp.key.readArmored($('#pubkey').text()).then(function(result) {
+				openpgp.config.commentstring = location.href;
+				options = {
+					message: openpgp.message.fromText(ENCRYPTION.area.val()),
+					publicKeys: result.keys,
+				};
+				openpgp.encrypt(options).then(function(ciphertext) {
+					ENCRYPTION.area.val(ciphertext.data);
+					ENCRYPTION.button.text(ENCRYPTION.button.data('copy'));
+					ENCRYPTION.button.off('click').click(function() {
+						ENCRYPTION.area.select();
+						if (document.queryCommandSupported('copy')) {
+							document.execCommand('copy');
+						}
+						ENCRYPTION.feedback.fadeIn('fast');
+					});
+				});
 			});
-		});
-	};
+		};
+	}
 	$('#copied .button, #copythis .button').click(function() {
 		ENCRYPTION.area.val('');
 		ENCRYPTION.reset();
 	});
-	ENCRYPTION.area.on('input', function(e) {
-		if (e.target.value === '') {
-			ENCRYPTION.reset();
-		}
-	});
-	ENCRYPTION.area.on(ENCRYPTION.loadEvents.area, ENCRYPTION.load);
-	ENCRYPTION.button
-		.on(ENCRYPTION.loadEvents.button, ENCRYPTION.load)
-		.removeAttr('title')
-		.prop('disabled', false);
+	if (ENCRYPTION.supported) {
+		ENCRYPTION.area.on('input', function(e) {
+			if (e.target.value === '') {
+				ENCRYPTION.reset();
+			}
+		});
+		ENCRYPTION.area.on(ENCRYPTION.loadEvents.area, ENCRYPTION.load);
+		ENCRYPTION.button
+			.on(ENCRYPTION.loadEvents.button, ENCRYPTION.load)
+			.removeAttr('title')
+			.prop('disabled', false);
+	} else {
+		ENCRYPTION.area.val(ENCRYPTION.button.data('unsupported')).prop('disabled', true);
+	}
 
 	if ($('#slides-container .highlight').length) {
 		$('html, body').animate({scrollTop: $('#slides-container .highlight').offset().top - 10});
