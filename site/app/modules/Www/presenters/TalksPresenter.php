6788fd40e1f8d90acc901556ccefa01176c78939
Michal Špaček <mail@michalspacek.cz>
2019-05-02 14:05:55 +0200

    Talks get sidebar layout too

--- site/app/modules/Www/presenters/TalksPresenter.php
+++ site/app/modules/Www/presenters/TalksPresenter.php
@@ -7,6 +7,7 @@ use MichalSpacekCz\Embed;
 use MichalSpacekCz\Formatter\Texy;
 use MichalSpacekCz\Talks;
 use MichalSpacekCz\Templating\Helpers;
+use MichalSpacekCz\Training\Dates;
 use Nette\Application\BadRequestException;
 use Nette\Application\UI\InvalidLinkException;
 use RuntimeException;
@@ -26,18 +27,17 @@ class TalksPresenter extends BasePresenter
 	/** @var Helpers */
 	protected $helpers;
 
+	/** @var Dates */
+	private $trainingDates;
 
-	public function __construct(
-		Texy $texyFormatter,
-		Talks $talks,
-		Embed $embed,
-		Helpers $helpers
-	)
+
+	public function __construct(Texy $texyFormatter, Talks $talks, Embed $embed, Helpers $helpers, Dates $trainingDates)
 	{
 		$this->texyFormatter = $texyFormatter;
 		$this->talks = $talks;
 		$this->embed = $embed;
 		$this->helpers = $helpers;
+		$this->trainingDates = $trainingDates;
 		parent::__construct();
 	}
 
@@ -88,6 +88,7 @@ class TalksPresenter extends BasePresenter
 		$this->template->slideNo = $slideNo;
 		$this->template->slides = $slides;
 		$this->template->ogImage = ($slides[$slideNo ?? 1]->image ?? ($talk->ogImage !== null ? sprintf($talk->ogImage, $slideNo ?? 1) : null));
+		$this->template->upcomingTrainings = $this->trainingDates->getPublicUpcoming();
 		foreach ($this->embed->getSlidesTemplateVars($talk, $slideNo) as $key => $value) {
 			$this->template->$key = $value;
 		}
