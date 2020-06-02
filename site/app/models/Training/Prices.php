97c3d03dfcb35feef35ee501583aea5680e90e86
Michal Špaček <mail@michalspacek.cz>
2020-06-02 04:53:39 +0200

    Student discount can be null

--- site/app/models/Training/Prices.php
+++ site/app/models/Training/Prices.php
@@ -22,13 +22,13 @@ class Prices
 	}
 
 
-	public function resolvePriceDiscountVat(int $price, int $studentDiscount, string $status, string $note): Price
+	public function resolvePriceDiscountVat(int $price, ?int $studentDiscount, string $status, string $note): Price
 	{
 		if (in_array($status, [Statuses::STATUS_NON_PUBLIC_TRAINING, Statuses::STATUS_TENTATIVE], true)) {
 			return new Price(null, null, null);
 		}
 
-		if (stripos($note, 'student') !== false) {
+		if (stripos($note, 'student') !== false && $studentDiscount !== null) {
 			return new Price((int)($price * (100 - $studentDiscount) / 100), $studentDiscount, $this->vatRate);
 		}
 
