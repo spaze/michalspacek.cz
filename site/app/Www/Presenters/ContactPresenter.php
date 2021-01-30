1c3af0eee8cb4796c56895f4a0b4593d03971fc5
Michal Špaček <mail@michalspacek.cz>
2021-01-30 05:13:05 +0100

    Make PGP encryption less prominent on a separate page and link to Signal

--- site/app/Www/Presenters/ContactPresenter.php
+++ site/app/Www/Presenters/ContactPresenter.php
@@ -6,25 +6,9 @@ namespace MichalSpacekCz\Www\Presenters;
 class ContactPresenter extends BasePresenter
 {
 
-	/**
-	 * Physical location root directory, no trailing slash.
-	 *
-	 * @var string
-	 */
-	private $locationRoot;
-
-
 	public function renderDefault(): void
 	{
 		$this->template->pageTitle  = $this->translator->translate('messages.title.contact');
-		$this->template->keyFile = $keyFile = 'key.asc';
-		$this->template->key = file_get_contents("{$this->locationRoot}/{$keyFile}");
-	}
-
-
-	public function setLocationRoot(string $locationRoot): void
-	{
-		$this->locationRoot = $locationRoot;
 	}
 
 }
---
+++ site/app/Www/Presenters/PgpPresenter.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types = 1);
+
+namespace MichalSpacekCz\Www\Presenters;
+
+class PgpPresenter extends BasePresenter
+{
+
+	/**
+	 * Physical location root directory, no trailing slash.
+	 *
+	 * @var string
+	 */
+	private string $locationRoot;
+
+
+	public function renderDefault(): void
+	{
+		$this->template->pageTitle  = $this->translator->translate('messages.title.encryptedmessages');
+		$this->template->keyFile = $keyFile = 'key.asc';
+		$this->template->key = file_get_contents("{$this->locationRoot}/{$keyFile}");
+	}
+
+
+	public function setLocationRoot(string $locationRoot): void
+	{
+		$this->locationRoot = $locationRoot;
+	}
+
+}
