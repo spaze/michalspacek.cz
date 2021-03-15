7bddf019abceefc8029f88a8dad66a7bd9962036
Michal Špaček <mail@michalspacek.cz>
2021-03-15 23:16:13 +0100

    De-jQuery-ize encryption
    The /pgp page is loading jQuery anyway but as encryption.js is loaded async, then jQuery might not be available when it finishes loading.

--- site/public/www.michalspacek.cz/i/js/encryption.js
+++ site/public/www.michalspacek.cz/i/js/encryption.js
@@ -1,76 +1,82 @@
-$(document).ready(function() {
-	var ENCRYPTION = ENCRYPTION || {};
-	ENCRYPTION.supported = Boolean(window.Promise);
-	ENCRYPTION.feedback = $(document.queryCommandSupported('copy') ? '#copied' : '#copythis');
-	ENCRYPTION.button = $('#encrypt');
-	ENCRYPTION.area = $('#message');
-	ENCRYPTION.loadEvents = {
-		button: 'click focus mouseover',
-		area: 'click focus'
+document.addEventListener('DOMContentLoaded', function() {
+	const supported = Boolean(window.Promise);
+	let encrypted = false;
+	const feedback = document.querySelector(document.queryCommandSupported('copy') ? '#copied' : '#copythis');
+	const button = document.querySelector('#encrypt');
+	const area = document.querySelector('#message');
+	const reset = function() {
+		encrypted = false;
+		button.innerText = button.dataset.encrypt;
+		button.removeEventListener('click', copy);
+		button.addEventListener('click', handler);
+		feedback.style.opacity = '0';
 	};
-	ENCRYPTION.reset = function() {
-		ENCRYPTION.button.text(ENCRYPTION.button.data('encrypt'));
-		ENCRYPTION.button.off('click').on('click', ENCRYPTION.handler);
-		ENCRYPTION.feedback.fadeOut('fast');
-	};
-	ENCRYPTION.load = function(e) {
-		ENCRYPTION.button.attr('title', ENCRYPTION.button.data('loading'));
-		document.getElementsByTagName('head')[0].appendChild(
-			$(document.createElement('script'))
-				.prop('async', true)
-				.attr('integrity', ENCRYPTION.button.data('integrity'))
-				.attr('src', ENCRYPTION.button.data('lib'))
-				.attr('crossorigin', 'anonymous')
-				.on('load', function() {
-					ENCRYPTION.area.off(ENCRYPTION.loadEvents.area);
-					ENCRYPTION.button
-						.off(ENCRYPTION.loadEvents.button)
-						.one('click', ENCRYPTION.handler)
-						.removeAttr('title');
-					if (e.target === ENCRYPTION.button[0] && e.type === 'click') {
-						ENCRYPTION.handler();
-					}
-				})[0]
-		);
-	};
-	if (ENCRYPTION.supported) {
-		ENCRYPTION.handler = function() {
-			openpgp.key.readArmored($('#pubkey').text()).then(function(result) {
-				openpgp.config.commentstring = location.href;
-				options = {
-					message: openpgp.message.fromText(ENCRYPTION.area.val()),
-					publicKeys: result.keys,
-				};
-				openpgp.encrypt(options).then(function(ciphertext) {
-					ENCRYPTION.area.val(ciphertext.data);
-					ENCRYPTION.button.text(ENCRYPTION.button.data('copy'));
-					ENCRYPTION.button.off('click').on('click', function() {
-						ENCRYPTION.area.select();
-						if (document.queryCommandSupported('copy')) {
-							document.execCommand('copy');
-						}
-						ENCRYPTION.feedback.fadeIn('fast');
-					});
-				});
+	const handler = function() {
+		if (encrypted || !supported) {
+			return;
+		}
+		openpgp.key.readArmored(document.querySelector('#pubkey').innerText).then(function(result) {
+			openpgp.config.commentstring = location.href;
+			let options = {
+				message: openpgp.message.fromText(area.value),
+				publicKeys: result.keys,
+			};
+			openpgp.encrypt(options).then(function(ciphertext) {
+				encrypted = true;
+				area.value = ciphertext.data;
+				button.innerText = button.dataset.copy;
+				button.removeEventListener('click', handler);
+				button.addEventListener('click', copy);
 			});
-		};
-	}
-	$('#copied .button, #copythis .button').on('click', function() {
-		ENCRYPTION.area.val('');
-		ENCRYPTION.reset();
-	});
-	if (ENCRYPTION.supported) {
-		ENCRYPTION.area.on('input', function(e) {
-			if (e.target.value === '') {
-				ENCRYPTION.reset();
+		});
+	};
+	const load = function(e) {
+		button.title = button.dataset.loading;
+		let script = document.createElement('script');
+		script.async = true;
+		script.integrity = button.dataset.integrity;
+		script.src = button.dataset.lib;
+		script.crossorigin = 'anonymous';
+		script.addEventListener('load', function() {
+			area.removeEventListener('click', load);
+			area.removeEventListener('focus', load);
+			button.removeEventListener('click', load);
+			button.removeEventListener('focus', load);
+			button.removeEventListener('mouseover', load);
+			button.addEventListener('click', handler)
+			button.removeAttribute('title');
+			if (e.target === button[0] && e.type === 'click') {
+				handler();
 			}
 		});
-		ENCRYPTION.area.on(ENCRYPTION.loadEvents.area, ENCRYPTION.load);
-		ENCRYPTION.button
-			.on(ENCRYPTION.loadEvents.button, ENCRYPTION.load)
-			.removeAttr('title')
-			.prop('disabled', false);
+		document.getElementsByTagName('head')[0].appendChild(script);
+	};
+	const copy = function() {
+		area.select();
+		if (document.queryCommandSupported('copy')) {
+			document.execCommand('copy');
+		}
+		feedback.style.opacity = '1';
+	};
+	document.querySelector('#copied .button, #copythis .button').addEventListener('click', function() {
+		area.value = '';
+		reset();
+	});
+	if (supported) {
+		area.addEventListener('input', function(e) {
+			if (e.target.value === '') {
+				reset();
+			}
+		});
+		area.addEventListener('click', load);
+		area.addEventListener('focus', load);
+		button.addEventListener('click', load);
+		button.addEventListener('focus', load);
+		button.addEventListener('mouseover', load);
+		button.removeAttribute('title');
+		button.removeAttribute('disabled');
 	} else {
-		ENCRYPTION.area.val(ENCRYPTION.button.data('unsupported')).prop('disabled', true);
+		area.value = button.dataset.unsupported;
+		area.disabled = true;
 	}
 });
