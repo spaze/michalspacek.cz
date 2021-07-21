2be0cf581f9ec7111b2b6d1cbe40aed62345a11f
Michal Špaček <mail@michalspacek.cz>
2021-07-21 03:57:35 +0200

    Move form data logging into a reusable service

--- site/app/Training/FormSpam.php
+++ site/app/Training/FormSpam.php
@@ -3,12 +3,33 @@ declare(strict_types = 1);
 
 namespace MichalSpacekCz\Training;
 
+use Nette\Http\SessionSection;
 use stdClass;
+use UnexpectedValueException;
 
 class FormSpam
 {
 
-	public function isSpam(stdClass $values): bool
+
+	private FormDataLogger $formDataLogger;
+
+
+	public function __construct(FormDataLogger $formDataLogger)
+	{
+		$this->formDataLogger = $formDataLogger;
+	}
+
+
+	public function check(stdClass $values, string $name, SessionSection $sessionSection): void
+	{
+		if ($this->isSpam($values)) {
+			$this->formDataLogger->log($values, $name, $sessionSection);
+			throw new UnexpectedValueException('Spammy application');
+		}
+	}
+
+
+	private function isSpam(stdClass $values): bool
 	{
 		if (preg_match('~\s+href="\s*https?://~', $values->note ?? '')) {
 			return true;
