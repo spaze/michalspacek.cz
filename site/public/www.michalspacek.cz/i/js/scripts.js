1e81a47980a4bc4b3b109c8b7d8809d3251cf170
Michal Špaček <mail@michalspacek.cz>
2021-12-19 05:06:30 +0100

    Remove jQuery from www scripts

--- site/public/www.michalspacek.cz/i/js/scripts.js
+++ site/public/www.michalspacek.cz/i/js/scripts.js
@@ -1,76 +1,115 @@
-$(document).ready(function() {
-	$('#termin a[href="#prihlaska"]').on('click', function() {
-		$('#frm-application-trainingId').val($(this).data('id'));
-	});
+document.addEventListener('DOMContentLoaded', function () {
+	const dateElement = document.querySelector('#termin a[href="#prihlaska"]');
+	if (dateElement) {
+		dateElement.addEventListener('click', function () {
+			document.querySelector('#frm-application-trainingId').value = this.dataset.id;
+		});
+	}
 
-	var APPLICATION = APPLICATION || {};
-	APPLICATION.hideLoadControls = function() {
-		$('#loadDataControls span').addClass('hidden');
+	const countryElement = document.querySelector('#frm-application-country');
+	const companyIdElement = document.querySelector('#frm-application-companyId');
+	const APPLICATION = {};
+	APPLICATION.hideLoadControls = function () {
+		document.querySelectorAll('#loadDataControls span').forEach(function (item) {
+			item.classList.add('hidden');
+		});
 	};
-	APPLICATION.showLoadControls = function(selector) {
-		$(selector).removeClass('hidden');
+	APPLICATION.showLoadControls = function (selector) {
+		document.querySelectorAll(selector).forEach(function (item) {
+			item.classList.remove('hidden');
+		});
 	};
-	APPLICATION.loadData = function(event) {
+	APPLICATION.loadData = function (event) {
 		event.preventDefault();
-		if ($('#frm-application-country').val() == '' || $('#frm-application-companyId').val().replace(/ /g, '') == '') {
-			alert($('#errorCountryCompanyMissing').text());
+		if (!countryElement || countryElement.value === '' || companyIdElement.value.replace(/ /g, '') === '') {
+			alert(document.querySelector('#errorCountryCompanyMissing').innerText);
 			return;
 		}
 		APPLICATION.hideLoadControls();
 		APPLICATION.showLoadControls('#loadDataWait');
-		var load = $.ajax({
-			url: $('#loadData').data('url'),
-			data: {
-				country: $('#frm-application-country').val(),
-				companyId: $('#frm-application-companyId').val().replace(/ /g, ''),
-			},
-			timeout: 10000
-		});
-		load.done(function(data) {
-			APPLICATION.hideLoadControls();
-			APPLICATION.showLoadControls('#loadDataAgain');
-			if (data.status == 200) {
-				$.each(['companyId', 'companyTaxId', 'company', 'street', 'city', 'zip', 'country'], function(key, value) {
-					$('#company').find('#frm-application-' + value).val(data[value]);
-				});
-			} else if (data.status == 400) {
-				APPLICATION.showLoadControls('#loadDataNotFound');
-			} else {
-				APPLICATION.showLoadControls('#loadDataError');
+		const loadDataElement = document.querySelector('#loadData')
+		if (!loadDataElement) {
+			return;
+		}
+
+		const url = new URL(loadDataElement.dataset.url);
+		url.searchParams.set('country', countryElement ? countryElement.value : '');
+		url.searchParams.set('companyId', companyIdElement ? companyIdElement.value.replace(/ /g, '') : '');
+		const controller = new AbortController();
+		const timeoutId = setTimeout(() => controller.abort(), 10000);
+		fetch(url.toString(), {signal: controller.signal})
+			.then((response) => {
+				clearTimeout(timeoutId);
+				if (!response.ok) {
+					throw new Error('Network response not ok');
+				}
+				return response.json()
+			})
+			.then((data) => {
+				APPLICATION.hideLoadControls();
+				APPLICATION.showLoadControls('#loadDataAgain');
+				if (data.status === 200) {
+					['companyId', 'companyTaxId', 'company', 'street', 'city', 'zip', 'country'].forEach(function (value) {
+						const companyElement = document.querySelector('#company');
+						if (companyElement) {
+							companyElement.querySelector('#frm-application-' + value).value = data[value];
+						}
+					});
+				} else if (data.status === 400) {
+					APPLICATION.showLoadControls('#loadDataNotFound');
+				} else {
+					APPLICATION.showLoadControls('#loadDataError');
+				}
+			})
+			.catch((error) => {
+				APPLICATION.hideLoadControls();
+				APPLICATION.showLoadControls('#loadDataAgain, #loadDataError');
+				console.error('🐶⚾ fetch error:', error);
+			});
+	};
+	document.querySelectorAll('#loadData a, #loadDataAgain a').forEach(function (item) {
+		item.addEventListener('click', APPLICATION.loadData);
+	});
+	if (companyIdElement) {
+		companyIdElement.addEventListener('keypress', function (e) {
+			if (e.which === 13) {
+				APPLICATION.loadData(e);
 			}
 		});
-		load.fail(function() {
-			APPLICATION.hideLoadControls();
-			APPLICATION.showLoadControls('#loadDataAgain, #loadDataError');
-		});
-	};
-	$('#loadData a, #loadDataAgain a').on('click', APPLICATION.loadData);
-	$('#frm-application-companyId').on('keypress', function(e) {
-		if (e.which === 13) {
-			APPLICATION.loadData(e);
-		}
-	});
-	$('#loadDataDisabled').addClass('hidden');
+	}
+	const loadDataDisabledElement = document.querySelector('#loadDataDisabled');
+	if (loadDataDisabledElement) {
+		loadDataDisabledElement.classList.add('hidden');
+	}
 	APPLICATION.hideLoadControls();
 	APPLICATION.showLoadControls('#loadDataControls, #loadData');
-	APPLICATION.changeLabels = function() {
-		$('#frm-application').find('label').each(function() {
-			var label = $(this).data('label-' + $('#frm-application-country').val());
-			if (label) {
-				$(this).text(label);
+	APPLICATION.changeLabels = function () {
+		document.querySelectorAll('#frm-application label').forEach(function (item) {
+			if (countryElement) {
+				let label = item.dataset[countryElement.value];
+				if (label) {
+					item.innerText = label;
+				}
 			}
 		});
 	};
-	$('#frm-application-country').change(APPLICATION.changeLabels);
+	if (countryElement) {
+		countryElement.addEventListener('change', APPLICATION.changeLabels);
+	}
 	APPLICATION.changeLabels();
 
-	if ($('#slides-container .highlight').length) {
-		$('html, body').animate({scrollTop: $('#slides-container .highlight').offset().top - 10});
+	const columnContentElement = document.querySelector('.column-content');
+	if (columnContentElement && window.location.hash) {
+		const columnContentHighlightElement = columnContentElement.querySelector(window.location.hash);
+		if (columnContentHighlightElement) {
+			columnContentHighlightElement.classList.add('highlight');
+		}
 	}
 
-	var para = $('.column-content').find(window.location.hash)
-	if (para.length) {
-		para.addClass('highlight');
-		$('html, body').animate({scrollTop: para.offset().top - 10});
+	const highlightElement = document.querySelector('.column-content .highlight');
+	if (highlightElement) {
+		highlightElement.scrollIntoView({
+			'behavior': 'smooth',
+		});
 	}
 });
