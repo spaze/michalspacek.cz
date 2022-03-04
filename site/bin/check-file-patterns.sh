45243f543ec9b91c283c06f9c2976581feaad2e8
Michal Špaček <mail@michalspacek.cz>
2022-03-04 21:36:23 +0100

    Add test for bad file patterns
    Patterns like:
    - tests using .php extension
    - tests without Test class name suffix
    - app files using .phpt (test) extension

---
+++ site/bin/check-file-patterns.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Wrong file patterns:"
+
+APP_FILES=$(find app/ -regex ".*\.phpt?$")
+BAD_APP_FILES=$(echo "$APP_FILES" | grep --invert-match "\.php$")
+TEST_FILES=$(find tests/ -mindepth 2 -regex ".*\.phpt?$")
+BAD_TEST_FILES=$(echo "$TEST_FILES" | grep --invert-match "Test.phpt")
+
+COUNT_APP_FILES=$(echo "$APP_FILES" | wc -l)
+COUNT_TEST_FILES=$(echo "$TEST_FILES" | wc -l)
+
+if [ -z "$BAD_APP_FILES$BAD_TEST_FILES" ]; then
+	echo "None, all ok ($COUNT_APP_FILES app files, $COUNT_TEST_FILES test files)"
+	exit 0
+else
+	echo "$BAD_APP_FILES$BAD_TEST_FILES"
+	echo "($COUNT_APP_FILES app files, $COUNT_TEST_FILES test files)"
+	exit 1
+fi
