82e8c99a1da924734e153f1225750a203209a0c4
Michal Špaček <mail@michalspacek.cz>
2022-06-08 02:12:13 +0200

    Convert UPC SSID form to a form factory

---
+++ site/app/Form/UpcKeysSsidFormFactory.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+namespace MichalSpacekCz\Form;
+
+use MichalSpacekCz\UpcKeys\UpcKeys;
+use Nette\Application\UI\Form;
+use stdClass;
+
+class UpcKeysSsidFormFactory
+{
+
+	public function __construct(
+		private readonly UnprotectedFormFactory $factory,
+		private readonly UpcKeys $upcKeys,
+	) {
+	}
+
+
+	/**
+	 * @param callable(string): void $onSuccess
+	 * @param callable $onError
+	 * @param string|null $ssid
+	 * @return Form
+	 */
+	public function create(callable $onSuccess, callable $onError, ?string $ssid): Form
+	{
+		$form = $this->factory->create();
+		$form->addText('ssid', 'SSID:')
+			->setHtmlAttribute('placeholder', $this->upcKeys->getSsidPlaceholder())
+			->setHtmlAttribute('title', '"UPC" and 7 digits')
+			->setDefaultValue($ssid)
+			->setRequired('Please enter an SSID')
+			->addRule($form::PATTERN, 'Wi-Fi network name has to be "UPC" and 7 digits (UPC1234567)', '\s*' . $this->upcKeys->getValidSsidPattern() . '\s*');
+		$form->addSubmit('submit', 'Get keys')
+			->setHtmlId('submit')
+			->setHtmlAttribute('data-alt', 'Wait…');
+		$form->onSuccess[] = function (Form $form, stdClass $values) use ($onSuccess, $onError): void {
+			$ssid = strtoupper(trim($values->ssid));
+			if (!$this->upcKeys->saveKeys($ssid)) {
+				$onError();
+			} else {
+				$onSuccess($ssid);
+			}
+		};
+		return $form;
+	}
+
+}
