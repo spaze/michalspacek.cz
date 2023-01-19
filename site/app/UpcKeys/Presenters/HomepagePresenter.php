9add5c2958940e33ec8d89f834d20ac11e3ff396
Michal Špaček <mail@michalspacek.cz>
2023-01-19 04:30:30 +0100

    Inline loadKeys() to make more template vars always available

--- site/app/UpcKeys/Presenters/HomepagePresenter.php
+++ site/app/UpcKeys/Presenters/HomepagePresenter.php
@@ -6,7 +6,6 @@ namespace MichalSpacekCz\UpcKeys\Presenters;
 use MichalSpacekCz\Form\UpcKeysSsidFormFactory;
 use MichalSpacekCz\UpcKeys\UpcKeys;
 use MichalSpacekCz\UpcKeys\WiFiBand;
-use MichalSpacekCz\UpcKeys\WiFiKey;
 use MichalSpacekCz\Www\Presenters\BasePresenter;
 use Nette\Application\BadRequestException;
 use Nette\Forms\Form;
@@ -38,18 +37,29 @@ class HomepagePresenter extends BasePresenter
 	public function actionDefault(?string $ssid = null, string $format = 'html'): void
 	{
 		$this->ssid = $ssid;
-		$keys = $this->loadKeys();
+		if ($this->ssid !== null) {
+			if ($this->upcKeys->isValidSsid($this->ssid)) {
+				if ($this->ssid !== strtoupper($this->ssid)) {
+					$this->redirect('this', strtoupper($this->ssid));
+				}
+				$keys = $this->upcKeys->getKeys($this->ssid);
+				if (!$keys) {
+					$error = 'Oops, something went wrong, please try again in a moment';
+				}
+			} else {
+				$error = 'Wi-Fi network name is not "UPC" and 7 numbers, the password cannot be recovered by this tool';
+				$this->httpResponse->setCode(IResponse::S404_NotFound);
+			}
+		}
+
+		$this->template->ssid = $this->ssid;
+		$this->template->error = $error ?? null;
+		$this->template->keys = isset($keys) && !isset($error) ? $keys : null;
 		switch ($format) {
 			case 'text':
-				foreach ($keys as $key => $value) {
-					$this->template->$key = $value;
-				}
 				$this->setView('text');
 				break;
 			case 'html':
-				foreach ($keys as $key => $value) {
-					$this->template->$key = $value;
-				}
 				$this->template->filterTypes = array_map(function (WiFiBand $band): string {
 					return $band->getLabel();
 				}, WiFiBand::getKnown());
@@ -58,40 +68,15 @@ class HomepagePresenter extends BasePresenter
 				$this->template->placeholder = $this->upcKeys->getSsidPlaceholder();
 				break;
 			case 'json':
-				$this->sendJson($keys);
+				$this->sendJson(array_filter([
+					'ssid' => $this->ssid,
+					'error' => $error ?? null,
+					'keys' => $keys ?? null,
+				]));
 				// no break, Presenter::sendJson() is in earlyTerminatingMethodCalls defined in the phpstan-nette extension config
 			default:
 				throw new BadRequestException('Unknown format');
 		}
-	}
-
-
-	/**
-	 * Check if SSID is valid and load keys
-	 *
-	 * @return array{}|array{ssid: string, error: string}|array{ssid: string, keys: non-empty-array<int, WiFiKey>}
-	 */
-	protected function loadKeys(): array
-	{
-		$result = [];
-		if ($this->ssid !== null) {
-			$result['ssid'] = $this->ssid;
-			if ($this->upcKeys->isValidSsid($this->ssid)) {
-				if ($this->ssid !== strtoupper($this->ssid)) {
-					$this->redirect('this', strtoupper($this->ssid));
-				}
-				$keys = $this->upcKeys->getKeys($this->ssid);
-				if (!$keys) {
-					$result['error'] = 'Oops, something went wrong, please try again in a moment';
-				} else {
-					$result['keys'] = $keys;
-				}
-			} else {
-				$result['error'] = 'Wi-Fi network name is not "UPC" and 7 numbers, the password cannot be recovered by this tool';
-				$this->httpResponse->setCode(IResponse::S404_NotFound);
-			}
-		}
-		return $result;
 	}
 
 
