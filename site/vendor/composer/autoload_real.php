5605b1422e1eb461199fe02d7714d9082b0b8ad7
Michal Špaček <mail@michalspacek.cz>
2023-02-25 04:03:49 +0100

    Update Composer files

--- site/vendor/composer/autoload_real.php
+++ site/vendor/composer/autoload_real.php
@@ -34,15 +34,15 @@ class ComposerAutoloaderInit247de957f14f643f393d210a332dd05b
         $loader->register(true);
 
         $filesToLoad = \Composer\Autoload\ComposerStaticInit247de957f14f643f393d210a332dd05b::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
             if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
                 $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
 
                 require $file;
             }
-        };
+        }, null, null);
         foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+            $requireFile($fileIdentifier, $file);
         }
 
         return $loader;
