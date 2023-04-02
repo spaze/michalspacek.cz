af62e9db54bd23d1ca5a53b0039e165258819c1e
Michal Špaček <mail@michalspacek.cz>
2023-04-03 00:46:25 +0200

    Use let/const instead of var

--- site/public/www.michalspacek.cz/i/js/remove-fbclid.js
+++ site/public/www.michalspacek.cz/i/js/remove-fbclid.js
@@ -1,13 +1,13 @@
 (function () {
-	var param = 'fbclid';
+	const param = 'fbclid';
 	if (location.search.indexOf(param + '=') !== -1) {
-		var replace = '';
+		let replace;
 		try {
-			var url = new URL(location);
+			const url = new URL(location);
 			url.searchParams.delete(param);
 			replace = url.href;
 		} catch (ex) {
-			var regExp = new RegExp('[?&]' + param + '=.*$');
+			const regExp = new RegExp('[?&]' + param + '=.*$');
 			replace = location.search.replace(regExp, '');
 			replace = location.pathname + replace + location.hash;
 		}
