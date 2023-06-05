d2891a1387c0298fe9a613c125423ecc0d0f2357
Michal Lulco <lulco@efabrica.sk>
2023-06-05 13:32:48 +0200

    Update packages
     - phpstan/phpstan updated from 1.10.15 to 1.10.16 patch
       See changes: https://github.com/phpstan/phpstan/compare/1.10.15...1.10.16
       Release notes: https://github.com/phpstan/phpstan/releases/tag/1.10.16
    
     - efabrica/phpstan-latte updated from 0.12.0 to 0.13.0 minor
       See changes: https://github.com/efabrica-team/phpstan-latte/compare/0.12.0...0.13.0
       Release notes: https://github.com/efabrica-team/phpstan-latte/releases/tag/0.13.0
    
     - roave/security-advisories updated from dev-latest@4dac388 to dev-latest@0feb297
       See changes: https://github.com/Roave/SecurityAdvisories/compare/4dac388...0feb297

---
+++ site/vendor/efabrica/phpstan-latte/src/LatteContext/Collector/FormControlCollector.php
@@ -0,0 +1,149 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Efabrica\PHPStanLatte\LatteContext\Collector;
+
+use Efabrica\PHPStanLatte\LatteContext\CollectedData\Form\CollectedFormControl;
+use Efabrica\PHPStanLatte\PhpDoc\LattePhpDocResolver;
+use Efabrica\PHPStanLatte\Resolver\NameResolver\NameResolver;
+use Efabrica\PHPStanLatte\Resolver\ValueResolver\ValueResolver;
+use Efabrica\PHPStanLatte\Template\Form\Container;
+use Efabrica\PHPStanLatte\Template\Form\Field;
+use PhpParser\Node;
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\ObjectType;
+
+/**
+ * @extends AbstractLatteContextCollector<CollectedFormControl>
+ */
+final class FormControlCollector extends AbstractLatteContextCollector
+{
+    private ValueResolver $valueResolver;
+
+    private LattePhpDocResolver $lattePhpDocResolver;
+
+    public function __construct(
+        NameResolver $nameResolver,
+        ReflectionProvider $reflectionProvider,
+        ValueResolver $valueResolver,
+        LattePhpDocResolver $lattePhpDocResolver
+    ) {
+        parent::__construct($nameResolver, $reflectionProvider);
+        $this->valueResolver = $valueResolver;
+        $this->lattePhpDocResolver = $lattePhpDocResolver;
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [MethodCall::class];
+    }
+
+    /**
+     * @param MethodCall $node
+     * @phpstan-return null|CollectedFormControl[]
+     */
+    public function collectData(Node $node, Scope $scope): ?array
+    {
+        $classReflection = $scope->getClassReflection();
+        if ($classReflection === null) {
+            return null;
+        }
+
+        $methodName = $scope->getFunctionName();
+        if ($methodName === null) {
+            return null;
+        }
+
+        if (!$node instanceof MethodCall) {
+            return null;
+        }
+
+        $formType = $scope->getType($node->var);
+
+        if (!$formType instanceof ObjectType) {
+            return null;
+        }
+
+        if (!$formType->isInstanceOf('Nette\Forms\Container')->yes()) {
+            return null;
+        }
+
+        if ($this->lattePhpDocResolver->resolveForNode($node, $scope)->isIgnored()) {
+            return null;
+        }
+
+        /** @var non-empty-string|null $formMethodName */
+        $formMethodName = $this->nameResolver->resolve($node->name);
+        if ($formMethodName === null) {
+            return null;
+        }
+
+        if ($formMethodName === 'addComponent') {
+            $componentArg = $node->getArgs()[0] ?? null;
+            if ($componentArg === null) {
+                return null;
+            }
+            $formControlType = $scope->getType($componentArg->value);
+            $controlNameArg = $node->getArgs()[1] ?? null;
+            $controlNameDefault = null;
+        } else {
+            // other form methods
+            $formClassReflection = $this->reflectionProvider->getClass($formType->getClassName());
+            if (!$formClassReflection->hasMethod($formMethodName)) {
+                return null;
+            }
+
+            $formControlReflectionMethod = $formClassReflection->getMethod($formMethodName, $scope);
+
+            $formControlParametersAcceptor = $formControlReflectionMethod->getVariants()[0] ?? null;
+            if ($formControlParametersAcceptor === null) {
+                return null;
+            }
+
+            $formControlType = $formControlParametersAcceptor->getReturnType();
+            $controlNameArg = $node->getArgs()[0] ?? null;
+
+            $formControlParameters = $formControlParametersAcceptor->getParameters();
+            $controlNameDefaultType = isset($formControlParameters[0]) ? $formControlParameters[0]->getDefaultValue() : null;
+            if ($controlNameDefaultType instanceof ConstantStringType) {
+                $controlNameDefault = trim($controlNameDefaultType->getValue(), '"\'');
+            } else {
+                $controlNameDefault = null;
+            }
+        }
+
+        if ($controlNameArg !== null) {
+            $controlNames = $this->valueResolver->resolveStringsOrInts($controlNameArg->value, $scope);
+            if ($controlNames === null) {
+                return null;
+            }
+        } elseif ($controlNameDefault !== null) {
+            $controlNames = [$controlNameDefault];
+        } else {
+            return null;
+        }
+
+        $formControls = [];
+        foreach ($controlNames as $controlName) {
+            $controlName = (string)$controlName;
+            if ((new ObjectType('Nette\Forms\Container'))->isSuperTypeOf($formControlType)->yes() && !(new ObjectType('Nette\Forms\Form'))->isSuperTypeOf($formControlType)->yes()) {
+                $formControl = new Container($controlName, $formControlType);
+            } elseif ((new ObjectType('Nette\Forms\Control'))->isSuperTypeOf($formControlType)->yes()) {
+                $formControl = new Field($controlName, $formControlType);
+            } else {
+                continue;
+            }
+
+            $formControls[] = new CollectedFormControl(
+                $classReflection->getName(),
+                $methodName,
+                $formControl
+            );
+        }
+        return $formControls;
+    }
+}
---
+++ site/vendor/efabrica/phpstan-latte/src/LatteContext/Finder/FormControlFinder.php
@@ -0,0 +1,91 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Efabrica\PHPStanLatte\LatteContext\Finder;
+
+use Efabrica\PHPStanLatte\Analyser\LatteContextData;
+use Efabrica\PHPStanLatte\LatteContext\CollectedData\Form\CollectedFormControl;
+use Efabrica\PHPStanLatte\Template\Form\ControlInterface;
+use Efabrica\PHPStanLatte\Template\ItemCombinator;
+use PHPStan\Reflection\ReflectionProvider;
+
+final class FormControlFinder
+{
+    /**
+     * @var array<string, array<string, ControlInterface[]>>
+     */
+    private array $assignedFormControls = [];
+
+    private ReflectionProvider $reflectionProvider;
+
+    private MethodCallFinder $methodCallFinder;
+
+    public function __construct(LatteContextData $latteContext, ReflectionProvider $reflectionProvider, MethodCallFinder $methodCallFinder)
+    {
+        $this->reflectionProvider = $reflectionProvider;
+        $this->methodCallFinder = $methodCallFinder;
+
+        $collectedFormControls = $latteContext->getCollectedData(CollectedFormControl::class);
+
+        /** @var CollectedFormControl $collectedFormControl */
+        foreach ($collectedFormControls as $collectedFormControl) {
+            $className = $collectedFormControl->getClassName();
+            $methodName = $collectedFormControl->getMethodName();
+            if (!isset($this->assignedFormControls[$className][$methodName])) {
+                $this->assignedFormControls[$className][$methodName] = [];
+            }
+            $this->assignedFormControls[$className][$methodName][] = $collectedFormControl->getFormControl();
+        }
+    }
+
+    /**
+     * @param class-string $className
+     * @return ControlInterface[]
+     */
+    public function find(string $className, string ...$methodNames): array
+    {
+        $foundFormControls = [
+            $this->findInClasses($className),
+            $this->findInMethodCalls($className, '__construct'),
+        ];
+        foreach ($methodNames as $methodName) {
+            $foundFormControls[] = $this->findInMethodCalls($className, $methodName);
+        }
+        return ItemCombinator::merge(...$foundFormControls);
+    }
+
+    /**
+     * @return ControlInterface[]
+     */
+    private function findInClasses(string $className): array
+    {
+        $classReflection = $this->reflectionProvider->getClass($className);
+
+        $assignedFormControls = $this->assignedFormControls[$className][''] ?? [];
+        foreach ($classReflection->getParentClassesNames() as $parentClass) {
+            $assignedFormControls = ItemCombinator::union($this->assignedFormControls[$parentClass][''] ?? [], $assignedFormControls);
+        }
+        return $assignedFormControls;
+    }
+
+    /**
+     * @param class-string $className
+     * @param ?class-string $currentClassName
+     * @return ControlInterface[]
+     */
+    private function findInMethodCalls(string $className, string $methodName, string $currentClassName = null): array
+    {
+        $callback = function (string $declaringClass, string $methodName, array $fromCalled, ?string $currentClassName) {
+            /** @var array<ControlInterface[]> $fromCalled */
+            /** @var ControlInterface[] $formControls */
+            $formControls = ItemCombinator::resolveTemplateTypes(
+                $this->assignedFormControls[$declaringClass][$methodName] ?? [],
+                $declaringClass,
+                $currentClassName
+            );
+            return ItemCombinator::union($formControls, ...$fromCalled);
+        };
+        return $this->methodCallFinder->traverseCalled($callback, $className, $methodName, $currentClassName);
+    }
+}
