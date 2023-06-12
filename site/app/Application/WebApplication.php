a59e6525b80e440bdcab89df862d0076a72572b9
Michal Špaček <mail@michalspacek.cz>
2023-06-13 01:40:13 +0200

    Use existing objects to get the URL to redirect to
    For #147, "Part $uri (mixed) of encapsed string cannot be cast to string", also `(string)$_SERVER['REQUEST_URI']` gives "Cannot cast mixed to string" which is... exciting!

--- site/app/Application/WebApplication.php
+++ site/app/Application/WebApplication.php
@@ -6,12 +6,14 @@ namespace MichalSpacekCz\Application;
 use MichalSpacekCz\Http\ContentSecurityPolicy\CspValues;
 use MichalSpacekCz\Http\SecurityHeaders;
 use Nette\Application\Application;
+use Nette\Http\IRequest;
 use Nette\Http\IResponse;
 
 class WebApplication
 {
 
 	public function __construct(
+		private readonly IRequest $httpRequest,
 		private readonly IResponse $httpResponse,
 		private readonly SecurityHeaders $securityHeaders,
 		private readonly Application $application,
@@ -32,10 +34,10 @@ class WebApplication
 
 	private function redirectToSecure(): void
 	{
-		$uri = $_SERVER['REQUEST_URI'];
 		if ($_SERVER['HTTP_HOST'] !== $this->fqdn) {
 			$this->securityHeaders->sendHeaders(CspValues::Default);
-			$this->httpResponse->redirect("https://{$this->fqdn}{$uri}", IResponse::S301_MovedPermanently);
+			$url = $this->httpRequest->getUrl()->withScheme('https')->withHost($this->fqdn);
+			$this->httpResponse->redirect($url->getAbsoluteUrl(), IResponse::S301_MovedPermanently);
 			exit();
 		}
 	}
