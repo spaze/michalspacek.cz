8dfddd60396f15e56da8f78b9f8722dc1824930e
Michal Špaček <mail@michalspacek.cz>
2023-07-22 03:22:04 +0200

    Display trainings on a venue page again
    This was a regression introduced in #143 in commit f0fc5462b5d6e3d4b8ee1a74de9e4664773a26b2 when I've converted included pages to a component and forgot there's no variable anymore. They were right, empty() shouldn't be considered harmless.

--- site/app/Training/DateList/UpcomingTrainingDatesList.php
+++ site/app/Training/DateList/UpcomingTrainingDatesList.php
@@ -3,12 +3,18 @@ declare(strict_types = 1);
 
 namespace MichalSpacekCz\Training\DateList;
 
+use Countable;
 use MichalSpacekCz\Application\UiControl;
+use MichalSpacekCz\Training\Dates\UpcomingTraining;
 use MichalSpacekCz\Training\Dates\UpcomingTrainingDates;
 use MichalSpacekCz\Training\FreeSeats;
 
-class UpcomingTrainingDatesList extends UiControl
+class UpcomingTrainingDatesList extends UiControl implements Countable
 {
+
+	/** @var array<string, UpcomingTraining>|null */
+	private ?array $upcomingTrainings = null;
+
 
 	public function __construct(
 		private readonly UpcomingTrainingDates $upcomingTrainingDates,
@@ -22,13 +28,31 @@ class UpcomingTrainingDatesList extends UiControl
 
 	public function render(): void
 	{
-		$upcomingTrainings = $this->venueId ? $this->upcomingTrainingDates->getPublicUpcomingAtVenue($this->venueId) : $this->upcomingTrainingDates->getPublicUpcoming();
-		if ($this->excludeTraining) {
-			unset($upcomingTrainings[$this->excludeTraining]);
-		}
+		$upcomingTrainings = $this->getUpcomingTrainingDates();
 		$this->template->lastFreeSeats = $this->showLastFreeSeats && $this->freeSeats->lastFreeSeatsAnyTraining($upcomingTrainings);
 		$this->template->upcomingTrainings = $upcomingTrainings;
 		$this->template->render(__DIR__ . '/upcomingTrainingDatesList.latte');
+	}
+
+
+	/**
+	 * @return array<string, UpcomingTraining>
+	 */
+	private function getUpcomingTrainingDates(): array
+	{
+		if ($this->upcomingTrainings === null) {
+			$this->upcomingTrainings = $this->venueId ? $this->upcomingTrainingDates->getPublicUpcomingAtVenue($this->venueId) : $this->upcomingTrainingDates->getPublicUpcoming();
+			if ($this->excludeTraining) {
+				unset($this->upcomingTrainings[$this->excludeTraining]);
+			}
+		}
+		return $this->upcomingTrainings;
+	}
+
+
+	public function count(): int
+	{
+		return count($this->getUpcomingTrainingDates());
 	}
 
 }
