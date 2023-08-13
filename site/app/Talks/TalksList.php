a0de49bf2ea8e86c941221587c1b85e93e3c65ba
Michal Špaček <mail@michalspacek.cz>
2023-08-13 16:34:30 +0200

    Show a language & flag icon when a talk's locale is other than default

--- site/app/Talks/TalksList.php
+++ site/app/Talks/TalksList.php
@@ -3,16 +3,24 @@ declare(strict_types = 1);
 
 namespace MichalSpacekCz\Talks;
 
+use Contributte\Translation\Translator;
 use MichalSpacekCz\Application\UiControl;
 
 class TalksList extends UiControl
 {
+
+	public function __construct(
+		private readonly Translator $translator,
+	) {
+	}
+
 
 	/**
 	 * @param list<Talk> $talks
 	 */
 	public function render(array $talks): void
 	{
+		$this->template->defaultLocale = $this->translator->getDefaultLocale();
 		$this->template->talks = $talks;
 		$this->template->render(__DIR__ . '/talksList.latte');
 	}
