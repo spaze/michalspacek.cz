spaze/michalspacek.cz

commit a12a6cb03e81e3a8849275838f98508506cb0a4c

Display a direct link to the talk in the other locale, if available
And a generic "list all talks" link if not.

site/app/Www/Presenters/TalksPresenter.php
@@ -8,6 +8,7 @@ use MichalSpacekCz\Media\Exceptions\ContentTypeException;
 use MichalSpacekCz\Media\SlidesPlatform;
 use MichalSpacekCz\Talks\Exceptions\TalkDoesNotExistException;
 use MichalSpacekCz\Talks\Exceptions\UnknownSlideException;
+use MichalSpacekCz\Talks\TalkLocaleUrls;
 use MichalSpacekCz\Talks\Talks;
 use MichalSpacekCz\Talks\TalkSlides;
 use MichalSpacekCz\Talks\TalksList;
@@ -20,9 +21,14 @@ use Nette\Http\IResponse;
 class TalksPresenter extends BasePresenter
 {
 
+	/** @var array<string, array{name: string}> */
+	private array $localeLinkParams = [];
+
+
 	public function __construct(
 		private readonly Talks $talks,
 		private readonly TalkSlides $talkSlides,
+		private readonly TalkLocaleUrls $talkLocaleUrls,
 		private readonly UpcomingTrainingDates $upcomingTrainingDates,
 		private readonly TalksListFactory $talksListFactory,
 		private readonly LocaleLinkGeneratorInterface $localeLinkGenerator,
@@ -76,6 +82,9 @@ class TalksPresenter extends BasePresenter
 		} catch (UnknownSlideException | TalkDoesNotExistException $e) {
 			throw new BadRequestException($e->getMessage(), previous: $e);
 		}
+		foreach ($this->talkLocaleUrls->get($talk) as $locale => $action) {
+			$this->localeLinkParams[$locale] = ['name' => $action];
+		}
 
 		$this->template->pageTitle = $this->talks->pageTitle('messages.title.talk', $talk);
 		$this->template->pageHeader = $talk->getTitle();
@@ -86,6 +95,21 @@ class TalksPresenter extends BasePresenter
 		$this->template->upcomingTrainings = $this->upcomingTrainingDates->getPublicUpcoming();
 		$this->template->video = $talk->getVideo()->setLazyLoad(count($slides) > 3);
 		$this->template->slidesPlatform = $talk->getSlidesHref() ? SlidesPlatform::tryFromUrl($talk->getSlidesHref())?->getName() : null;
+	}
+
+
+	protected function getLocaleLinkAction(): string
+	{
+		return (count($this->localeLinkParams) > 1 ? parent::getLocaleLinkAction() : 'Www:Talks:');
+	}
+
+
+	/**
+	 * @return array<string, array{name: string}>
+	 */
+	protected function getLocaleLinkParams(): array
+	{
+		return $this->localeLinkParams;
 	}
 
 

site/app/Talks/TalkLocaleUrls.php
@@ -1,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+
+namespace MichalSpacekCz\Talks;
+
+use Nette\Database\Explorer;
+
+class TalkLocaleUrls
+{
+
+	public function __construct(
+		private readonly Explorer $database,
+	) {
+	}
+
+
+	/**
+	 * @return array<string, string> locale => action
+	 */
+	public function get(Talk $talk): array
+	{
+		if (!$talk->getTranslationGroupId()) {
+			return [];
+		}
+		return $this->database->fetchPairs(
+			'SELECT l.locale, t.action FROM talks t JOIN locales l ON t.key_locale = l.id_locale WHERE t.key_translation_group = ?',
+			$talk->getTranslationGroupId(),
+		);
+	}
+
+}

site/config/services.neon
@@ -84,6 +84,7 @@ services:
 	- MichalSpacekCz\Tags\Tags
 	- MichalSpacekCz\Talks\TalkFactory(videoFactory: @talkVideoFactory)
 	- MichalSpacekCz\Talks\TalkInputsFactory(videoThumbnails: @talkVideoThumbnails)
+	- MichalSpacekCz\Talks\TalkLocaleUrls
 	- MichalSpacekCz\Talks\Talks
 	- MichalSpacekCz\Talks\TalkSlides
 	- MichalSpacekCz\Talks\TalksListFactory

site/tests/Talks/TalkLocaleUrlsTest.phpt
@@ -1,0 +1,85 @@
+<?php
+declare(strict_types = 1);
+
+namespace MichalSpacekCz\Talks;
+
+use DateTime;
+use MichalSpacekCz\Media\Video;
+use MichalSpacekCz\Test\Database\Database;
+use Nette\Utils\Html;
+use Tester\Assert;
+use Tester\TestCase;
+
+$runner = require __DIR__ . '/../bootstrap.php';
+
+/** @testCase */
+class TalkLocaleUrlsTest extends TestCase
+{
+
+	public function __construct(
+		private readonly TalkLocaleUrls $talkLocaleUrls,
+		private readonly Database $database,
+	) {
+	}
+
+
+	public function testGet(): void
+	{
+		$expected = ['cs_CZ' => 'foobar'];
+		$this->database->setFetchPairsResult($expected);
+		Assert::same([], $this->talkLocaleUrls->get($this->buildTalk(null)));
+		Assert::same($expected, $this->talkLocaleUrls->get($this->buildTalk(1337)));
+	}
+
+
+	private function buildTalk(?int $translationGroup): Talk
+	{
+		$video = new Video(
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			320,
+			200,
+			null,
+		);
+		return new Talk(
+			10,
+			1,
+			'cs_CZ',
+			$translationGroup,
+			null,
+			null,
+			Html::fromText('title'),
+			'title',
+			null,
+			null,
+			new DateTime(),
+			null,
+			null,
+			false,
+			null,
+			null,
+			$video,
+			null,
+			Html::fromText('event'),
+			'event',
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			null,
+			false,
+		);
+	}
+
+}
+
+$runner->run(TalkLocaleUrlsTest::class);

commit 699a9069f2193f6d31f86854a277240e73fa9c59

Have to use parent classes to get current action/params for localeLinkGenerator because $this ones are overwritten
This was working before in #196 but I broke it in a12a6cb03e81e3a8849275838f98508506cb0a4c by adding getLocaleLinkAction() and getLocaleLinkParams()

site/app/Www/Presenters/TalksPresenter.php
@@ -65,7 +65,8 @@ class TalksPresenter extends BasePresenter
 		try {
 			$talk = $this->talks->get($name);
 			if ($talk->getLocale() !== $this->translator->getDefaultLocale()) {
-				$this->redirectUrl($this->localeLinkGenerator->links(...$this->getLocaleLinksGeneratorParams())[$talk->getLocale()]->getUrl(), IResponse::S301_MovedPermanently);
+				$links = $this->localeLinkGenerator->links(parent::getLocaleLinkAction(), parent::getLocaleLinkParams());
+				$this->redirectUrl($links[$talk->getLocale()]->getUrl(), IResponse::S301_MovedPermanently);
 			}
 			if ($talk->getSlidesTalkId()) {
 				$slidesTalk = $this->talks->getById($talk->getSlidesTalkId());