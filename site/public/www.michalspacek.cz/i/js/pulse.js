2cbbdcfc09a00781562bbfb2ec89e42988656a7d
Michal Špaček <mail@michalspacek.cz>
2024-02-29 22:47:23 +0100

    Remove `hidden` class from all next siblings in password storage history
    Close #280

--- site/public/www.michalspacek.cz/i/js/pulse.js
+++ site/public/www.michalspacek.cz/i/js/pulse.js
@@ -1,10 +1,18 @@
 App.ready(document, function () {
 	App.onClick('.open-button', function (event) {
 		event.preventDefault();
-		const element = this.dataset.open ? document.querySelector(this.dataset.open) : this.parentElement.nextElementSibling.matches('.expandable') ? this.parentElement.nextElementSibling : null;
-		if (element) {
-			element.classList.toggle('hidden');
+		let elements;
+		if (this.dataset.open) {
+			const element = document.querySelector(this.dataset.open);
+			if (element) {
+				elements = [element];
+			}
+		} else {
+			elements = App.nextElementSiblings(this.parentElement, '.expandable');
 		}
+		elements.forEach(function (item) {
+			item.classList.toggle('hidden');
+		});
 		this.classList.toggle('open');
 	});
 	App.onChange('#frm-searchSort select', function () {
