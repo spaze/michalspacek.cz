8315c1893451c4703f72aa25123522142886feeb
Michal Špaček <mail@michalspacek.cz>
2024-03-07 18:21:38 +0100

    Require nette/schema
    Used e.g. Nette\Schema\Processor in app/Tls/CertificatesApiClient.php:28 (+ 40 more)

--- site/vendor/composer/installed.php
+++ site/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'spaze/michalspacek.cz',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '9de8c3d9f670b17352abb6a535a4c8319e6c80dc',
+        'reference' => '6cc9a5a5d960324d0fc28515ea562dd795f2dead',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -474,7 +474,7 @@
         'spaze/michalspacek.cz' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '9de8c3d9f670b17352abb6a535a4c8319e6c80dc',
+            'reference' => '6cc9a5a5d960324d0fc28515ea562dd795f2dead',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
