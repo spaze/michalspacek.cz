41f77bf46fc97cbcb810153b42b276447e05e933
Michal Špaček <mail@michalspacek.cz>
2024-11-27 02:58:11 +0100

    Set exec bit for files that have the exec bit in Git

--- app/bin/set-file-mode-bits.sh
+++ app/bin/set-file-mode-bits.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-SITEDIR="$(dirname "$0")/.."
-chmod --verbose a+w "$SITEDIR/../uploads/" "$SITEDIR/log/" "$SITEDIR/temp/" "$SITEDIR/public/www.michalspacek.cz/i/build/"
+# Writable dirs
+ROOT_DIR="$(realpath "$(dirname "$0")/../..")"
+chmod --verbose a+w "$ROOT_DIR/uploads/" "$ROOT_DIR/app/log/" "$ROOT_DIR/app/temp/" "$ROOT_DIR/app/public/www.michalspacek.cz/i/build/"
+
+# Executable files
+FILES=$(git ls-files --stage | grep "^100755" | grep --only-matching --perl-regexp "(?<=\t).*$")
+for FILE in $FILES; do
+	chmod a+x "$FILE"
+done
