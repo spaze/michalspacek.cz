fffdba641a42fca2d8ab42ee2a0908fdaa4d1eab
Michal Špaček <mail@michalspacek.cz>
2026-03-23 00:17:07 +0100

    More readable chmod command

--- app/bin/set-file-mode-bits.sh
+++ app/bin/set-file-mode-bits.sh
@@ -2,7 +2,11 @@
 
 # Writable dirs
 ROOT_DIR="$(realpath "$(dirname "$0")/../..")"
-chmod --verbose a+w "$ROOT_DIR/uploads/" "$ROOT_DIR/app/log/" "$ROOT_DIR/app/temp/" "$ROOT_DIR/app/public/www.michalspacek.cz/i/build/"
+chmod --verbose a+w \
+	"$ROOT_DIR/uploads/" \
+	"$ROOT_DIR/app/log/" \
+	"$ROOT_DIR/app/temp/" \
+	"$ROOT_DIR/app/public/www.michalspacek.cz/i/build/" \
 
 # Set sticky bit to the session directory and make it writable, even if unused
 chmod --verbose 1733 "$ROOT_DIR/sessions/"
