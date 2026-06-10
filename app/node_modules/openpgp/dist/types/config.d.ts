d6dbc650a2f53e601f72201340a0954a43242ada
Michal Špaček <mail@michalspacek.cz>
2026-06-10 14:22:34 +0200

    Actually update openpgp 6.2.2 to 6.3.1

---
+++ app/node_modules/openpgp/dist/types/config.d.ts
@@ -0,0 +1,58 @@
+/**
+ * Global configuration values
+ * @module config
+ * @access public
+ */
+import enums from './enums.ts';
+export interface Config {
+    preferredHashAlgorithm: enums.hash;
+    preferredSymmetricAlgorithm: enums.symmetric;
+    preferredCompressionAlgorithm: enums.compression;
+    showVersion: boolean;
+    showComment: boolean;
+    aeadProtect: boolean;
+    parseAEADEncryptedV4KeysAsLegacy: boolean;
+    allowUnauthenticatedMessages: boolean;
+    allowUnauthenticatedStream: boolean;
+    minRSABits: number;
+    passwordCollisionCheck: boolean;
+    ignoreUnsupportedPackets: boolean;
+    ignoreMalformedPackets: boolean;
+    enforceGrammar: boolean;
+    additionalAllowedPackets: Array<{
+        new (): any;
+        tag: enums.packet;
+    }>;
+    versionString: string;
+    commentString: string;
+    allowInsecureDecryptionWithSigningKeys: boolean;
+    allowInsecureVerificationWithReformattedKeys: boolean;
+    allowMissingKeyFlags: boolean;
+    constantTimePKCS1Decryption: boolean;
+    constantTimePKCS1DecryptionSupportedSymmetricAlgorithms: Set<enums.symmetric>;
+    v6Keys: boolean;
+    enableParsingV5Entities: boolean;
+    preferredAEADAlgorithm: enums.aead;
+    aeadChunkSizeByte: number;
+    s2kType: enums.s2k.iterated | enums.s2k.argon2;
+    s2kIterationCountByte: number;
+    s2kArgon2Params: {
+        passes: number;
+        parallelism: number;
+        memoryExponent: number;
+    };
+    maxArgon2MemoryExponent: number;
+    maxUserIDLength: number;
+    maxDecompressedMessageSize: number;
+    knownNotations: string[];
+    nonDeterministicSignaturesViaNotation: boolean;
+    useEllipticFallback: boolean;
+    rejectHashAlgorithms: Set<enums.hash>;
+    rejectMessageHashAlgorithms: Set<enums.hash>;
+    rejectPublicKeyAlgorithms: Set<enums.publicKey>;
+    rejectCurves: Set<enums.curve>;
+}
+declare const config: Config;
+export interface PartialConfig extends Partial<Config> {
+}
+export default config;
